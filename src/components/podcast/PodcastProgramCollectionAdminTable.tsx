ac751c40852eec071b4f082008bb1b8b4a407108
Elantris <j88107333@gmail.com>
2019-11-29 16:09:23 +0800

    [FEATURE] add custom filter

--- src/components/podcast/PodcastProgramCollectionAdminTable.tsx
+++ src/components/podcast/PodcastProgramCollectionAdminTable.tsx
@@ -1,6 +1,6 @@
-import { Table } from 'antd'
+import { Icon, Input, Table } from 'antd'
 import { ColumnProps } from 'antd/lib/table'
-import React from 'react'
+import React, { useState } from 'react'
 import styled from 'styled-components'
 import { currencyFormatter } from '../../helpers'
 import EmptyCover from '../../images/default/empty-cover.png'
@@ -34,7 +34,7 @@ const StyledText = styled.div`
   white-space: nowrap;
 `
 const StyledPriceLabel = styled.span`
-  color: #21b1b1;
+  color: ${props => props.theme['@primary-color']};
   font-weight: 500;
   letter-spacing: 0.2px;
   white-space: nowrap;
@@ -61,73 +61,107 @@ const StyledStatusLabel = styled.div<{ active?: boolean }>`
   }
 `
 
-const columns: ColumnProps<PodcastProgramProps>[] = [
-  {
-    title: '名稱',
-    dataIndex: 'title',
-    key: 'title',
-    width: '25rem',
-    render: (text, record, index) => (
-      <div key={record.id} className="d-flex align-items-center justify-content-between">
-        <CustomRatioImage
-          width="42px"
-          ratio={1}
-          src={record.coverUrl || EmptyCover}
-          className="mr-3 pr-2 flex-shrink-0"
-        />
-        <StyledTitle className="flex-grow-1">{record.title}</StyledTitle>
-      </div>
-    ),
-  },
-  {
-    title: '老師',
-    dataIndex: 'creator',
-    key: 'creator',
-    render: (text, record, index) => <StyledText key={record.id}>{text}</StyledText>,
-  },
-  {
-    title: '售價',
-    dataIndex: 'price',
-    key: 'price',
-    width: '12rem',
-    render: (text, record, index) => (
-      <div key={record.id}>
-        {typeof record.salePrice === 'number' && (
-          <StyledPriceLabel className="mr-2">{currencyFormatter(record.salePrice)}</StyledPriceLabel>
-        )}
-        <StyledPriceLabel>{currencyFormatter(record.listPrice)}</StyledPriceLabel>
-      </div>
-    ),
-  },
-  {
-    title: '購買',
-    dataIndex: 'salesCount',
-    key: 'salesCount',
-    width: '6rem',
-    align: 'right',
-    render: (text, record, index) => <StyledText key={record.id}>{text}</StyledText>,
-  },
-  {
-    title: '狀態',
-    dataIndex: 'status',
-    key: 'status',
-    width: '6rem',
-    render: (text, record, index) => (
-      <StyledStatusLabel
-        key={record.id}
-        active={record.isPublished}
-        className="d-flex align-items-center justify-content-start"
-      >
-        {record.isPublished ? '已發布' : '未發布'}
-      </StyledStatusLabel>
-    ),
-  },
-]
+const getColumnSearchProps: (
+  onSearch: (selectedKeys?: string[], confirm?: () => void) => void,
+) => ColumnProps<PodcastProgramProps> = onSearch => ({
+  filterDropdown: ({ setSelectedKeys, selectedKeys, confirm, clearFilters }) => (
+    <div className="p-2">
+      <Input
+        autoFocus
+        value={selectedKeys && selectedKeys[0]}
+        onChange={e => {
+          setSelectedKeys && setSelectedKeys([e.target.value || ''])
+          onSearch([e.target.value || ''], confirm)
+        }}
+        style={{ width: 188, marginBottom: 8, display: 'block' }}
+      />
+    </div>
+  ),
+  filterIcon: filtered => <Icon type="search" style={{ color: filtered ? 'var(--primary)' : undefined }} />,
+})
 
 const PodcastProgramCollectionAdminTable: React.FC<{
   podcastPrograms: PodcastProgramProps[]
 }> = ({ podcastPrograms }) => {
-  return <Table columns={columns} dataSource={podcastPrograms} />
+  const [titleSearch, setTitleSearch] = useState('')
+  const [nameSearch, setNameSearch] = useState('')
+
+  const data = podcastPrograms
+    .filter(podcastProgram => !titleSearch || podcastProgram.title.includes(titleSearch))
+    .filter(podcastProgram => !nameSearch || podcastProgram.creator.includes(nameSearch))
+
+  const columns: ColumnProps<PodcastProgramProps>[] = [
+    {
+      title: '名稱',
+      dataIndex: 'title',
+      key: 'title',
+      width: '25rem',
+      render: (text, record, index) => (
+        <div key={record.id} className="d-flex align-items-center justify-content-between">
+          <CustomRatioImage
+            width="42px"
+            ratio={1}
+            src={record.coverUrl || EmptyCover}
+            className="mr-3 pr-2 flex-shrink-0"
+          />
+          <StyledTitle className="flex-grow-1">{record.title}</StyledTitle>
+        </div>
+      ),
+      ...getColumnSearchProps(selectedKeys => {
+        selectedKeys && setTitleSearch(selectedKeys[0] || '')
+        setNameSearch('')
+      }),
+    },
+    {
+      title: '老師',
+      dataIndex: 'creator',
+      key: 'creator',
+      render: (text, record, index) => <StyledText key={record.id}>{text}</StyledText>,
+      ...getColumnSearchProps(selectedKeys => {
+        selectedKeys && setNameSearch(selectedKeys[0] || '')
+        setTitleSearch('')
+      }),
+    },
+    {
+      title: '售價',
+      dataIndex: 'price',
+      key: 'price',
+      width: '12rem',
+      render: (text, record, index) => (
+        <div key={record.id}>
+          {typeof record.salePrice === 'number' && (
+            <StyledPriceLabel className="mr-2">{currencyFormatter(record.salePrice)}</StyledPriceLabel>
+          )}
+          <StyledPriceLabel>{currencyFormatter(record.listPrice)}</StyledPriceLabel>
+        </div>
+      ),
+    },
+    {
+      title: '購買',
+      dataIndex: 'salesCount',
+      key: 'salesCount',
+      width: '6rem',
+      align: 'right',
+      render: (text, record, index) => <StyledText key={record.id}>{text}</StyledText>,
+    },
+    {
+      title: '狀態',
+      dataIndex: 'status',
+      key: 'status',
+      width: '6rem',
+      render: (text, record, index) => (
+        <StyledStatusLabel
+          key={record.id}
+          active={record.isPublished}
+          className="d-flex align-items-center justify-content-start"
+        >
+          {record.isPublished ? '已發布' : '未發布'}
+        </StyledStatusLabel>
+      ),
+    },
+  ]
+
+  return <Table columns={columns} dataSource={data} />
 }
 
 export default PodcastProgramCollectionAdminTable
