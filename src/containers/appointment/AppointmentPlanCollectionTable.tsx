5f0c088814c4fd15420fd37e4926b29cecabb9c8
Elantris <j88107333@gmail.com>
2019-12-18 16:41:28 +0800

    [FEATURE] implement collection table and creation modal

--- src/containers/appointment/AppointmentPlanCollectionTable.tsx
+++ src/containers/appointment/AppointmentPlanCollectionTable.tsx
@@ -1,68 +1,53 @@
-import React from 'react'
+import { useQuery } from '@apollo/react-hooks'
+import gql from 'graphql-tag'
+import React, { useEffect } from 'react'
 import AppointmentPlanCollectionTableComponent, {
   AppointmentPlanProps,
 } from '../../components/appointment/AppointmentPlanCollectionTable'
+import types from '../../types'
 
 const AppointmentPlanCollectionTable: React.FC = () => {
-  // ! fake data
-  const appointmentPlans: AppointmentPlanProps[] = [
-    {
-      id: 'appointment-plan-1',
-      creatorName: '小美',
-      title: 'XXXXXX服務',
-      duration: 60,
-      listPrice: 500,
-      enrollment: 1,
-      isPublished: true,
-    },
-    {
-      id: 'appointment-plan-2',
-      creatorName: '小美',
-      title: '面試諮詢',
-      duration: 120,
-      listPrice: 500,
-      enrollment: 0,
-      isPublished: false,
-    },
-    {
-      id: 'appointment-plan-3',
-      creatorName: 'Alex Wang',
-      title: '商業分析顧問諮詢商業分析顧我是很長的方案名稱方案名稱方案名稱方案名',
-      duration: 20,
-      listPrice: 290,
-      enrollment: 12,
-      isPublished: true,
-    },
-    {
-      id: 'appointment-plan-4',
-      creatorName: '王麗莉',
-      title: 'OOOOO服務',
-      duration: 30,
-      listPrice: 6400,
-      enrollment: 0,
-      isPublished: false,
-    },
-    {
-      id: 'appointment-plan-5',
-      creatorName: '陳一',
-      title: 'XX服務',
-      duration: 30,
-      listPrice: 500,
-      enrollment: 23,
-      isPublished: true,
-    },
-    {
-      id: 'appointment-plan-6',
-      creatorName: '李美',
-      title: 'OO諮詢',
-      duration: 60,
-      listPrice: 500,
-      enrollment: 4,
-      isPublished: true,
-    },
-  ]
+  const { loading, error, data, refetch } = useQuery<types.GET_APPOINTMENT_PLAN_COLLECTION_ADMIN>(
+    GET_APPOINTMENT_PLAN_COLLECTION_ADMIN,
+  )
 
-  return <AppointmentPlanCollectionTableComponent appointmentPlans={appointmentPlans} />
+  useEffect(() => {
+    refetch()
+  }, [])
+
+  const appointmentPlans: AppointmentPlanProps[] =
+    loading || !!error || !data
+      ? []
+      : data.appointment_plan.map(appointmentPlan => ({
+          id: appointmentPlan.id,
+          avatarUrl: appointmentPlan.creator.picture_url,
+          creatorName: appointmentPlan.creator.name || appointmentPlan.creator.username,
+          title: appointmentPlan.title,
+          duration: appointmentPlan.duration,
+          listPrice: appointmentPlan.price,
+          enrollment: 0,
+          isPublished: !!appointmentPlan.publishedAt,
+        }))
+
+  return <AppointmentPlanCollectionTableComponent appointmentPlans={appointmentPlans} loading={loading} />
 }
+
+const GET_APPOINTMENT_PLAN_COLLECTION_ADMIN = gql`
+  query GET_APPOINTMENT_PLAN_COLLECTION_ADMIN {
+    appointment_plan(order_by: { updated_at: desc }) {
+      id
+      creator {
+        id
+        picture_url
+        name
+        username
+      }
+      title
+      duration
+      price
+      publishedAt
+    }
+  }
+`
 
 export default AppointmentPlanCollectionTable
