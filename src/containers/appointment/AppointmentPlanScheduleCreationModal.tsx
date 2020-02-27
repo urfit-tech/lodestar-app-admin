78ff656e67e6fcd599a7cae657a3952debf7cdc5
futian <pcsh810778@gmail.com>
2020-02-27 10:56:03 +0800

    [FEATURE] add translation of taipei

--- src/containers/appointment/AppointmentPlanScheduleCreationModal.tsx
+++ src/containers/appointment/AppointmentPlanScheduleCreationModal.tsx
@@ -92,7 +92,10 @@ const AppointmentPlanScheduleCreationModal: React.FC<FormComponentProps> = ({ fo
           </Button>
           <StyledTimeStandardBlock>
             {formatMessage(appointmentMessages.text.timezone, {
-              city: momentTz.tz.guess().split('/')[1],
+              city:
+                momentTz.tz.guess().split('/')[1] === 'Taipei'
+                  ? formatMessage(appointmentMessages.text.taipei)
+                  : momentTz.tz.guess().split('/')[1],
               timezone: moment()
                 .zone(momentTz.tz.guess())
                 .format('Z'),
@@ -106,7 +109,10 @@ const AppointmentPlanScheduleCreationModal: React.FC<FormComponentProps> = ({ fo
           <div className="mb-3">{formatMessage(appointmentMessages.label.createPeriod)}</div>
           <StyledTimeZoneBlock>
             {formatMessage(appointmentMessages.text.timezone, {
-              city: momentTz.tz.guess().split('/')[1],
+              city:
+                momentTz.tz.guess().split('/')[1] === 'Taipei'
+                  ? formatMessage(appointmentMessages.text.taipei)
+                  : momentTz.tz.guess().split('/')[1],
               timezone: moment()
                 .zone(momentTz.tz.guess())
                 .format('Z'),
