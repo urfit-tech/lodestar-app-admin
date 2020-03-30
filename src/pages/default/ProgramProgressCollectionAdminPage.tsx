9ec4e65bc6bb05a37dcffcf494164bf2d71ee6d6
futian <pcsh810778@gmail.com>
2020-03-30 13:12:45 +0800

    [FEATURE] get program package enrollment data

--- src/pages/default/ProgramProgressCollectionAdminPage.tsx
+++ src/pages/default/ProgramProgressCollectionAdminPage.tsx
@@ -1,14 +1,17 @@
-import { Icon, Typography } from 'antd'
+import { useQuery } from '@apollo/react-hooks'
+import { Icon, Select, Typography } from 'antd'
+import { SelectProps } from 'antd/lib/select'
+import gql from 'graphql-tag'
 import React, { useContext, useState } from 'react'
 import { useIntl } from 'react-intl'
 import { AdminPageBlock } from '../../components/admin'
 import CreatorAdminLayout from '../../components/layout/CreatorAdminLayout'
 import OwnerAdminLayout from '../../components/layout/OwnerAdminLayout'
 import ProgramProgressTable from '../../containers/program/ProgramProgressTable'
-import ProgramSelector from '../../containers/program/ProgramSelector'
 import AppContext from '../../contexts/AppContext'
 import { useAuth } from '../../contexts/AuthContext'
 import { commonMessages } from '../../helpers/translation'
+import types from '../../types'
 import LoadingPage from './LoadingPage'
 import NotFoundPage from './NotFoundPage'
 
@@ -45,5 +48,46 @@ const ProgramProgressCollectionAdminPage: React.FC = () => {
     </AdminLayout>
   )
 }
+
+const ProgramSelector: React.FC<SelectProps & {
+  allText?: string
+}> = ({ allText, ...selectProps }) => {
+  const { formatMessage } = useIntl()
+  const { loading, error, data: programs } = useProgramContentEnrollment()
+
+  return (
+    <Select disabled={!!error} loading={loading} style={{ width: '100%' }} defaultValue="all" {...selectProps}>
+      <Select.Option key="all">{allText || formatMessage(commonMessages.label.allProgram)}</Select.Option>
+      {programs.map(program => (
+        <Select.Option key={program.id}>{program.title}</Select.Option>
+      ))}
+    </Select>
+  )
+}
+
+const useProgramContentEnrollment = () => {
+  const { loading, error, data } = useQuery<types.GET_PROGRAM_CONTENT_ENROLLMENT>(GET_PROGRAM_CONTENT_ENROLLMENT)
+
+  return {
+    loading,
+    error,
+    data:
+      data?.program_content_enrollment.map(programContentEnrollment => ({
+        id: programContentEnrollment.program_id,
+        title: programContentEnrollment?.program?.title || '',
+      })) || [],
+  }
+}
+
+const GET_PROGRAM_CONTENT_ENROLLMENT = gql`
+  query GET_PROGRAM_CONTENT_ENROLLMENT {
+    program_content_enrollment(where: { program: { published_at: { _is_null: false } } }, distinct_on: program_id) {
+      program_id
+      program {
+        title
+      }
+    }
+  }
+`
 
 export default ProgramProgressCollectionAdminPage
