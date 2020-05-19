d61362aa05f288d043a8ef732eed63412803b3a0
Wayne Huang <wayne123tw [at] gmail com>
2020-05-20 00:58:09 +0800

    [FEATURE] rebase develop

---
+++ src/components/layout/OwnerAdminLayout.tsx
@@ -0,0 +1,42 @@
+import { Button } from 'antd'
+import React from 'react'
+import { defineMessages, useIntl } from 'react-intl'
+import { Link } from 'react-router-dom'
+import { StyledContent } from '.'
+import { useRouteKeys } from '../../hooks/util'
+import { OwnerAdminMenu } from '../admin/AdminMenu'
+import Responsive from '../common/Responsive'
+import DefaultLayout from './DefaultLayout'
+
+const messages = defineMessages({
+  ownerBackstage: { id: 'layout.label.ownerBackstage', defaultMessage: '平台管理者專區' },
+})
+
+const OwnerAdminLayout: React.FC = ({ children }) => {
+  const { formatMessage } = useIntl()
+  const defaultSelectedKeys = useRouteKeys()
+
+  return (
+    <DefaultLayout
+      noFooter
+      renderTitle={() => (
+        <Link to={`/`} className="d-flex">
+          <Button type="link">{formatMessage(messages.ownerBackstage)}</Button>
+        </Link>
+      )}
+    >
+      <div className="d-flex">
+        <Responsive.Desktop>
+          <StyledContent noFooter white>
+            <OwnerAdminMenu defaultSelectedKeys={defaultSelectedKeys} />
+          </StyledContent>
+        </Responsive.Desktop>
+        <StyledContent className="flex-grow-1 p-3 p-sm-5" noFooter>
+          {children}
+        </StyledContent>
+      </div>
+    </DefaultLayout>
+  )
+}
+
+export default OwnerAdminLayout
---
+++ src/components/program/ProgramCategorySelector.tsx
@@ -0,0 +1,83 @@
+import { useQuery } from '@apollo/react-hooks'
+import { Button, Select } from 'antd'
+import gql from 'graphql-tag'
+import React, { useContext } from 'react'
+import styled from 'styled-components'
+import { InferType } from 'yup'
+import AppContext from '../../contexts/AppContext'
+import { categorySchema } from '../../schemas/program'
+import types from '../../types'
+
+const StyledButton = styled(Button)`
+  padding: 0 20px;
+  font-size: 14px;
+`
+const StyledSelect = styled(Select)<{ value?: any; onChange?: any }>`
+  width: 100%;
+  && .ant-select-selection__choice {
+    padding-right: 2rem;
+    background: var(--gray-lighter);
+    color: var(--gray-darker);
+  }
+  .ant-select-selection--multiple .ant-select-selection__choice {
+    border: none;
+    border-radius: 4px;
+  }
+  .ant-select-selection--multiple .ant-select-selection__choice__remove {
+    right: 0.5rem;
+    color: #9b9b9b;
+  }
+`
+
+type ProgramCategorySelectorProps = {
+  value?: string
+  onChange?: (value: string) => void
+  flatten?: boolean
+}
+const ProgramCategorySelector: React.FC<ProgramCategorySelectorProps> = ({ flatten, value, onChange }, ref) => {
+  const { id: appId } = useContext(AppContext)
+  const { loading, data } = useQuery<types.GET_PROGRAM_CATEGORIES, types.GET_PROGRAM_CATEGORIESVariables>(
+    GET_PROGRAM_CATEGORIES,
+    {
+      variables: { appId },
+    },
+  )
+  const categories: InferType<typeof categorySchema>[] = (data && data.category) || []
+  return flatten ? (
+    <>
+      {categories.map(category => {
+        return (
+          <StyledButton
+            className="mr-2 mb-2"
+            key={category.id}
+            type={category.id === value ? 'primary' : 'default'}
+            shape="round"
+            onClick={() => onChange && onChange(category.id)}
+          >
+            {category.name}
+          </StyledButton>
+        )
+      })}
+    </>
+  ) : (
+    <StyledSelect ref={ref} mode="multiple" loading={loading} value={value} onChange={onChange}>
+      {categories.map(category => (
+        <Select.Option style={{ borderRadius: '4px' }} key={category.id}>
+          {category.name}
+        </Select.Option>
+      ))}
+    </StyledSelect>
+  )
+}
+
+export const GET_PROGRAM_CATEGORIES = gql`
+  query GET_PROGRAM_CATEGORIES($appId: String!) {
+    category(where: { app_id: { _eq: $appId }, class: { _eq: "program" } }, order_by: { position: asc }) {
+      id
+      name
+      position
+    }
+  }
+`
+
+export default React.forwardRef(ProgramCategorySelector)
