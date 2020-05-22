23765952f9e01b622ee0e22b2e087759005fb8c6
Elantris <j88107333@gmail.com>
2020-05-22 16:03:00 +0800

    [BUGFIX] rename shipping admin page

--- src/pages/default/MerchandiseShippingAdminPage.tsx
+++ src/pages/default/MerchandiseShippingAdminPage.tsx
@@ -6,17 +6,17 @@ import AdminLayout from '../../components/layout/AdminLayout'
 import { commonMessages } from '../../helpers/translation'
 import { ReactComponent as ShopIcon } from '../../images/icon/shop.svg'
 
-const MerchandiseShopCollectionAdminPage: React.FC = () => {
+const MerchandiseShippingAdminPage: React.FC = () => {
   const { formatMessage } = useIntl()
 
   return (
     <AdminLayout>
       <AdminPageTitle className="mb-4">
         <Icon component={() => <ShopIcon />} className="mr-2" />
-        <span>{formatMessage(commonMessages.menu.merchandiseShop)}</span>
+        <span>{formatMessage(commonMessages.menu.merchandiseShipping)}</span>
       </AdminPageTitle>
     </AdminLayout>
   )
 }
 
-export default MerchandiseShopCollectionAdminPage
+export default MerchandiseShippingAdminPage
--- src/pages/default/MerchandiseShippingPage.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import AdminLayout from '../../components/layout/AdminLayout'
-
-const MerchandiseShippingPage: React.FC = () => {
-  return <AdminLayout></AdminLayout>
-}
-
-export default MerchandiseShippingPage
