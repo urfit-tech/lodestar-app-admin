8943705d1a5ef9622001d8bf0d4bf788d344f660
Elantris <j88107333@gmail.com>
2020-05-25 18:21:21 +0800

    [BUGFIX] go back button in admin pages

--- src/containers/activity/ActivityHeader.tsx
+++ src/containers/activity/ActivityHeader.tsx
@@ -1,6 +1,6 @@
 import { Button, Icon } from 'antd'
 import React, { useContext } from 'react'
-import useRouter from 'use-react-router'
+import { Link } from 'react-router-dom'
 import { AdminHeader, AdminHeaderTitle } from '../../components/admin'
 import ActivityContext from '../../contexts/ActivityContext'
 import AppContext from '../../contexts/AppContext'
@@ -8,15 +8,16 @@ import AppContext from '../../contexts/AppContext'
 const ActivityHeader: React.FC<{
   activityId: string
 }> = ({ activityId }) => {
-  const { history } = useRouter()
   const { settings } = useContext(AppContext)
   const { activity } = useContext(ActivityContext)
 
   return (
     <AdminHeader>
-      <Button type="link" onClick={() => history.goBack()} className="mr-3">
-        <Icon type="arrow-left" />
-      </Button>
+      <Link to="/activities">
+        <Button type="link" className="mr-3">
+          <Icon type="arrow-left" />
+        </Button>
+      </Link>
 
       <AdminHeaderTitle>{activity ? activity.title : activityId}</AdminHeaderTitle>
       <a href={`//${settings['host']}/activities/${activityId}`} target="_blank" rel="noopener noreferrer">
