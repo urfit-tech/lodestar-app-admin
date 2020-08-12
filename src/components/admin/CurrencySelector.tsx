5d80808b775170b6b6bc51f480c250a0bb18a16b
徐愷 <kkshyu.tw@gmail.com>
2020-08-12 16:35:24 +0800

    [BUGFIX] add LSC to currency

--- src/components/admin/CurrencySelector.tsx
+++ src/components/admin/CurrencySelector.tsx
@@ -9,12 +9,15 @@ const CurrencySelector: React.FC<{
   const { currencies, enabledModules, settings } = useContext(AppContext)
   return (
     <Select style={{ width: 120 }} value={value} onChange={onChange}>
-      {Object.keys(currencies).map(currencyId => (
-        <Select.Option key={currencyId} value={currencyId}>
-          {currencies[currencyId].name}
-        </Select.Option>
-      ))}
-      {enabledModules.coin && <Select.Option value="LSC">{settings['coin.name'] || '極星幣'}</Select.Option>}
+      {Object.keys(currencies)
+        .filter(currencyId => currencyId !== 'LSC' || enabledModules.coin)
+        .map(currencyId => {
+          return (
+            <Select.Option key={currencyId} value={currencyId}>
+              {(currencyId === 'LSC' && settings['coin.name']) || currencies[currencyId].name}
+            </Select.Option>
+          )
+        })}
     </Select>
   )
 }
