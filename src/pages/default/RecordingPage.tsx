0d2f9d7489d9cdc4e85f25e121baf9b0a3c7ac91
Wayne Huang <wayne123tw@gmail.com>
2020-10-28 00:10:24 +0800

    [BUGFIX] select AudioTrack should not prompt loading

--- src/pages/default/RecordingPage.tsx
+++ src/pages/default/RecordingPage.tsx
@@ -153,8 +153,8 @@ const RecordingPage: React.FC = () => {
       try {
         const signedPodCastProgramAudios = await signPodCastProgramAudios(authToken, podcastProgramAdmin.audios)
         setSignedPodCastProgramAudios(signedPodCastProgramAudios)
-        if (!currentAudioId && signedPodCastProgramAudios.length) {
-          setCurrentAudioId(signedPodCastProgramAudios[0].id)
+        if (signedPodCastProgramAudios.length) {
+          setCurrentAudioId(currentAudioId => currentAudioId || signedPodCastProgramAudios[0].id)
         }
       } catch (error) {
         message.error(error.message)
@@ -164,7 +164,7 @@ const RecordingPage: React.FC = () => {
     }
 
     signAudios()
-  }, [authToken, podcastProgramAdmin, currentAudioId])
+  }, [authToken, podcastProgramAdmin])
 
   const audioTrackRefMap: Map<string, React.RefObject<AudioTrackCardRef>> = useMemo(() => {
     if (podcastAudios == null) {
