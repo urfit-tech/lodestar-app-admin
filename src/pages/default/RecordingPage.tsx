0a83b5e80d7cac78e7f3a70744a7908c9becaed7
Wayne Huang <wayne123tw@gmail.com>
2020-10-27 21:02:59 +0800

    [BUGFIX] recording cut naming fix

--- src/pages/default/RecordingPage.tsx
+++ src/pages/default/RecordingPage.tsx
@@ -299,12 +299,12 @@ const RecordingPage: React.FC = () => {
       return
     }
 
-    const [, originalFileName] = /^([^()]+)(.+)?$/.exec(audio.filename) || []
+    const [, originalFileName, ,] = /^([^()]+)\(?(\d+)?\)?\.(\w+)?$/.exec(audio.filename) || []
     const serialNumber =
       Math.max(
         ...signedPodCastProgramAudios
           .filter(audio => audio.filename.includes(originalFileName))
-          .map(audio => (/^([^.()]+)([(]+)([1-9])([)]+)?$/.exec(audio.filename) || [])[3] || '0')
+          .map(audio => (/^([^()]+)\(?(\d+)?\)?\.(\w+)?$/.exec(audio.filename) || [])[2] || '0')
           .map(Number),
       ) + 1
 
@@ -313,7 +313,7 @@ const RecordingPage: React.FC = () => {
     try {
       // eslint-disable-next-line @typescript-eslint/no-unused-vars
       const [audioId1, _] = await splitPodcastProgramAudio(authToken, appId, audio.id, currentPlayingSecond, {
-        filenames: [audio.filename, `${originalFileName}(${serialNumber})`],
+        filenames: [originalFileName, `${originalFileName}(${serialNumber})`],
       })
 
       setCurrentPlayingSecond(0)
