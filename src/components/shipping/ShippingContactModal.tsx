11c5c7ed344a23932e6a94699c60295a565f8dd0
FutianShen <pcsh810778@gmail.com>
2020-11-05 16:07:56 +0800

    [BUGFIX] fix editor style

--- src/components/shipping/ShippingContactModal.tsx
+++ src/components/shipping/ShippingContactModal.tsx
@@ -1,19 +1,31 @@
 import Icon from '@ant-design/icons'
 import { Button } from 'antd'
+import BraftEditor from 'braft-editor'
 import React, { useContext } from 'react'
 import { defineMessages, useIntl } from 'react-intl'
+import styled from 'styled-components'
 import AppContext from '../../contexts/AppContext'
 import { useAuth } from '../../contexts/AuthContext'
 import { commonMessages } from '../../helpers/translation'
 import { ReactComponent as IconMail } from '../../images/icon/email-o.svg'
 import AdminModal from '../admin/AdminModal'
 import { createUploadFn } from '../form/AdminBraftEditor'
-import { StyledEditor } from '../issue/IssueReplyCreationBlock'
 
 const messages = defineMessages({
   contactMessage: { id: 'merchandise.ui.contactMessage', defaultMessage: '聯絡訊息' },
   messageContent: { id: 'merchandise.label.messageContent', defaultMessage: '請填寫訊息內容' },
 })
+
+const StyledEditor = styled(BraftEditor)`
+  .bf-controlbar {
+    box-shadow: initial;
+  }
+  .bf-content {
+    border: 1px solid #cdcdcd;
+    border-radius: 4px;
+    height: initial;
+  }
+`
 
 const ShippingContactModal: React.FC = () => {
   const { formatMessage } = useIntl()
