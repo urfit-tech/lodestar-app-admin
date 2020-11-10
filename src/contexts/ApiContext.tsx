b37ef99e7e745b9943e73fe9c526ae16cb47d46c
ponchimeow <trancetoradora@gmail.com>
2020-11-10 18:10:30 +0800

    [FEATURE] remove update login at mutation

--- src/contexts/ApiContext.tsx
+++ src/contexts/ApiContext.tsx
@@ -1,30 +1,11 @@
 import { ApolloProvider } from '@apollo/react-hooks'
-import gql from 'graphql-tag'
-import React, { useEffect } from 'react'
+import React from 'react'
 import { createApolloClient } from '../helpers/apollo'
-import types from '../types'
 import { useAuth } from './AuthContext'
 
 export const ApiProvider: React.FC = ({ children }) => {
-  const { currentMemberId, authToken } = useAuth()
+  const { authToken } = useAuth()
   const apolloClient = createApolloClient({ authToken })
-  useEffect(() => {
-    currentMemberId &&
-      apolloClient.mutate<types.UPDATE_LOGINED_AT, types.UPDATE_LOGINED_ATVariables>({
-        mutation: UPDATE_LOGINED_AT,
-        variables: {
-          memberId: currentMemberId,
-          loginedAt: new Date(),
-        },
-      })
-  }, [apolloClient, currentMemberId])
+
   return <ApolloProvider client={apolloClient}>{children}</ApolloProvider>
 }
-
-const UPDATE_LOGINED_AT = gql`
-  mutation UPDATE_LOGINED_AT($memberId: String!, $loginedAt: timestamptz) {
-    update_member(where: { id: { _eq: $memberId } }, _set: { logined_at: $loginedAt }) {
-      affected_rows
-    }
-  }
-`
