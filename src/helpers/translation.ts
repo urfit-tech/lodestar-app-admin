b6e57da6b4d39d6c0d3c877d6021e23ad36a18c7
ponchimeow <trancetoradora@gmail.com>
2020-12-24 00:15:34 +0800

    [FEATURE] ProjectPlanAdminBlock, ProjectPlanAdminModal, ProjectPlanCatd

--- src/helpers/translation.ts
+++ src/helpers/translation.ts
@@ -66,6 +66,7 @@ export const commonMessages = {
     uploadImage: { id: 'common.ui.uploadImage', defaultMessage: '上傳圖片' },
     uploadCertification: { id: 'common.ui.uploadCertification', defaultMessage: '上傳證明' },
     createContract: { id: 'common.ui.createContract', defaultMessage: '建立合約' },
+    createPlan: { id: 'common.ui.createPlan', defaultMessage: '建立方案' },
   }),
   label: defineMessages({
     selectInstructor: { id: 'common.label.selectInstructor', defaultMessage: '選擇老師' },
