f2921ae1c81bdf7208d1b33a1f85dc4422ee2293
ponchimeow <trancetoradora@gmail.com>
2020-12-24 13:14:02 +0800

    [FEATURE] FileUploader ImageUploader

--- src/helpers/translation.ts
+++ src/helpers/translation.ts
@@ -67,6 +67,7 @@ export const commonMessages = {
     uploadCertification: { id: 'common.ui.uploadCertification', defaultMessage: '上傳證明' },
     createContract: { id: 'common.ui.createContract', defaultMessage: '建立合約' },
     createPlan: { id: 'common.ui.createPlan', defaultMessage: '建立方案' },
+    uploadImage: { id: 'common.ui.uploadImage', defaultMessage: '上傳圖片' },
   }),
   label: defineMessages({
     selectInstructor: { id: 'common.label.selectInstructor', defaultMessage: '選擇老師' },
