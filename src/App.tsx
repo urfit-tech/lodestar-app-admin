56596b943e340b5f2c7ee86c69d8434235669925
FutianShen <pcsh810778@gmail.com>
2021-01-22 18:50:26 +0800

    [FEATURE] add custom admin menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,11 @@
+import Icon from '@ant-design/icons'
+import { Menu } from 'antd'
 import Application from 'lodestar-app-admin/src/Application'
+import { StyledMenu } from 'lodestar-app-admin/src/components/admin/AdminMenu'
+import { isEmpty } from 'ramda'
 import React from 'react'
 import './App.scss'
+import { ReactComponent as UserCopyIcon } from './images/icons/user-copy.svg'
 import MemberCollectionAdminPage from './pages/MemberCollectionAdminPage'
 import MemberContractPage from './pages/MemberContractPage'
 import MemberNoteAdminPage from './pages/MemberNoteAdminPage'
@@ -10,6 +15,57 @@ const App = () => {
   return (
     <Application
       appId="xuemi"
+      customRender={{
+        renderAdminMenu: ({ menuItems, handleClick: onClick }) => {
+          return (
+            <StyledMenu
+              mode="inline"
+              defaultOpenKeys={menuItems.filter(v => !isEmpty(v.subMenuItems)).map(v => v.key)}
+              onClick={onClick}
+            >
+              {[
+                ...menuItems.slice(0, 14),
+                {
+                  permissionIsAllowed: true,
+                  key: 'member_contract_collection',
+                  icon: () => <UserCopyIcon />,
+                  name: '合約資料管理',
+                  subMenuItems: undefined,
+                },
+                ...menuItems.slice(15),
+              ]
+                .filter(v => v.permissionIsAllowed)
+                .map(v => {
+                  if (v.subMenuItems) {
+                    return (
+                      <Menu.SubMenu
+                        key={v.key}
+                        title={
+                          <span>
+                            <Icon component={v.icon} />
+                            <span>{v.name}</span>
+                          </span>
+                        }
+                      >
+                        {v.subMenuItems
+                          .filter(w => w.permissionIsAllowed)
+                          .map(w => (
+                            <Menu.Item key={w.key}>{w.name}</Menu.Item>
+                          ))}
+                      </Menu.SubMenu>
+                    )
+                  }
+                  return (
+                    <Menu.Item key={v.key}>
+                      <Icon component={v.icon} />
+                      <span>{v.name}</span>
+                    </Menu.Item>
+                  )
+                })}
+            </StyledMenu>
+          )
+        },
+      }}
       extraRouteProps={{
         owner_members: {
           path: '/admin/members',
@@ -21,7 +77,7 @@ const App = () => {
           pageName: <MemberNoteAdminPage />,
           authenticated: true,
         },
-        member_contract_creation: {
+        member_contract_collection: {
           path: '/member-contracts',
           pageName: <MemberContractPage />,
           authenticated: true,
