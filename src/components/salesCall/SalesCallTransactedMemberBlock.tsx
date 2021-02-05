d6bf25a7e8a8f14bebaa488b2d97857bdd65df57
FutianShen <pcsh810778@gmail.com>
2021-02-05 15:36:14 +0800

    [FEATURE] add renderMemberAdminLayout.sider

--- src/components/salesCall/SalesCallTransactedMemberBlock.tsx
+++ src/components/salesCall/SalesCallTransactedMemberBlock.tsx
@@ -56,6 +56,7 @@ const SalesCallTransactedMemberBlock: React.FC<{ salesId: string }> = ({ salesId
           value={selectedKeys[0]}
           onChange={e => setSelectedKeys(e.target.value ? [e.target.value] : [])}
           onPressEnter={() => {
+            confirm({ closeDropdown: true })
             onSetFilter(selectedKeys[0] as string)
           }}
           style={{ width: 188, marginBottom: 8, display: 'block' }}
@@ -64,6 +65,7 @@ const SalesCallTransactedMemberBlock: React.FC<{ salesId: string }> = ({ salesId
           <Button
             type="primary"
             onClick={() => {
+              confirm({ closeDropdown: true })
               onSetFilter(selectedKeys[0] as string)
             }}
             icon={<SearchOutlined />}
