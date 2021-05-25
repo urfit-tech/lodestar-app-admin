7a32aa75e38aef2944ab78cdea977be46f85c89d
Elantris <j88107333@gmail.com>
2021-05-25 19:16:17 +0800

    [FEATURE] add acsi langauge

--- src/contexts/LanguageContext.tsx
+++ src/contexts/LanguageContext.tsx
@@ -1,10 +1,10 @@
 import moment from 'moment'
 import 'moment/locale/zh-tw'
-import React, { createContext, useState } from 'react'
+import React, { createContext, useEffect, useState } from 'react'
 import { IntlProvider } from 'react-intl'
 import { useApp } from './AppContext'
 
-const supportedLanguages = ['zh', 'en', 'vi']
+const supportedLanguages = ['zh', 'zh-cn', 'en', 'vi', 'acsi']
 
 type LanguageProps = {
   currentLanguage: string
@@ -17,19 +17,37 @@ const defaultLanguage: LanguageProps = {
 const LanguageContext = createContext<LanguageProps>(defaultLanguage)
 
 export const LanguageProvider: React.FC = ({ children }) => {
-  const { enabledModules } = useApp()
+  const { enabledModules, settings } = useApp()
+  const [currentLanguage, setCurrentLanguage] = useState('zh')
+  const [locale, setLocale] = useState('zh')
+  moment.locale('zh-tw')
 
-  const browserLanguage = navigator.language.split('-')[0]
-  const cachedLanguage = localStorage.getItem('kolable.app.language')
-  const [currentLanguage, setCurrentLanguage] = useState(
-    typeof cachedLanguage === 'string' && supportedLanguages.includes(cachedLanguage)
-      ? cachedLanguage
-      : supportedLanguages.includes(browserLanguage)
-      ? browserLanguage
-      : 'zh',
-  )
+  useEffect(() => {
+    const browserLanguage = settings['language'] || navigator.language.split('-')[0]
+    const cachedLanguage = localStorage.getItem('kolable.app.language')
+    setCurrentLanguage(
+      enabledModules.locale
+        ? typeof cachedLanguage === 'string' && supportedLanguages.includes(cachedLanguage)
+          ? cachedLanguage
+          : supportedLanguages.includes(browserLanguage)
+          ? browserLanguage
+          : 'zh'
+        : 'zh',
+    )
+  }, [enabledModules, settings])
 
-  moment.locale(currentLanguage)
+  useEffect(() => {
+    switch (currentLanguage) {
+      case 'zh':
+      case 'acsi':
+        setLocale('zh')
+        moment.locale('zh-tw')
+        break
+      default:
+        setLocale(currentLanguage)
+        moment.locale(currentLanguage)
+    }
+  }, [currentLanguage])
 
   let messages: any = {}
   try {
@@ -38,33 +56,19 @@ export const LanguageProvider: React.FC = ({ children }) => {
     }
   } catch {}
 
-  const language: LanguageProps = {
-    currentLanguage,
-    setCurrentLanguage: (language: string) => {
-      if (supportedLanguages.includes(language)) {
-        localStorage.setItem('kolable.app.language', language)
-
-        setCurrentLanguage(language)
-
-        switch (language) {
-          case 'zh':
-            moment.locale('zh-tw')
-            break
-          default:
-            moment.locale(language)
-        }
-      }
-    },
-  }
-
   return (
-    <LanguageContext.Provider value={language}>
-      <IntlProvider
-        defaultLocale="zh"
-        locale={currentLanguage}
-        messages={messages}
-        onError={error => process.env.VERBOSE && console.error(error)}
-      >
+    <LanguageContext.Provider
+      value={{
+        currentLanguage,
+        setCurrentLanguage: (newLanguage: string) => {
+          if (supportedLanguages.includes(newLanguage)) {
+            localStorage.setItem('kolable.app.language', newLanguage)
+            setCurrentLanguage(newLanguage)
+          }
+        },
+      }}
+    >
+      <IntlProvider defaultLocale="zh" locale={locale} messages={messages}>
         {children}
       </IntlProvider>
     </LanguageContext.Provider>
