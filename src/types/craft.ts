6bd592a8d40a4d19868a6f5f65e784dacf799f62
ponchimeow <trancetoradora@gmail.com>
2021-07-08 09:42:22 +0800

    [FEATURE] add craft types, translation and style

--- src/types/craft.ts
+++ src/types/craft.ts
@@ -11,3 +11,30 @@ export type CraftPageColumnProps = {
   url: string
   updateAt: Date
 }
+
+export type CraftTextStyleProps = {
+  fontSize: number
+  lineHeight?: number
+  padding: number
+  textAlign: 'left' | 'right' | 'center'
+  fontWeight: 'lighter' | 'normal' | 'bold'
+  color: string
+}
+
+export type CraftTitleProps = {
+  titleContent: string
+} & CraftTextStyleProps
+
+export type CraftParagraphProps = {
+  paragraphContent: string
+} & CraftTextStyleProps
+
+export type CraftButtonProps = {
+  title: string
+  link: string
+  openNewTab: boolean
+  size: 'sm' | 'md' | 'lg'
+  block: boolean
+  variant: 'text' | 'solid' | 'outline'
+  color: string
+}
