68c442fa2260c603a2c55f108a5fae50dcf4b3b4
ponchimeow <trancetoradora@gmail.com>
2021-07-27 10:38:22 +0800

    [FEATURE] hide SketchPicker default

--- src/components/craft/CraftColorPickerBlock.tsx
+++ src/components/craft/CraftColorPickerBlock.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useIntl } from 'react-intl'
 import { craftPageMessages } from '../../helpers/translation'
 import { StyleCircleColorInput, StyledCraftSettingLabel, StyledSketchPicker, StyledUnderLineInput } from '../admin'
@@ -8,17 +8,26 @@ const CraftColorPickerBlock: React.VFC<{
   onChange?: (value: string) => void
 }> = ({ value, onChange }) => {
   const { formatMessage } = useIntl()
+  const [visible, setVisible] = useState(false)
 
   return (
     <>
       <StyledCraftSettingLabel>{formatMessage(craftPageMessages.label.color)}</StyledCraftSettingLabel>
-      <StyledUnderLineInput className="mb-3" bordered={false} value={value} />
+      <StyledUnderLineInput
+        className="mb-3"
+        bordered={false}
+        value={value}
+        onFocus={() => setVisible(true)}
+        onBlur={() => setVisible(false)}
+        onChange={e => onChange && onChange(e.target.value)}
+      />
+
       <div className="d-flex mb-3">
         <StyleCircleColorInput background="#e1614b" onClick={() => onChange && onChange('#e1614b')} />
         <StyleCircleColorInput className="ml-2" background="#585858" onClick={() => onChange && onChange('#585858')} />
         <StyleCircleColorInput className="ml-2" background="#ffffff" onClick={() => onChange && onChange('#ffffff')} />
       </div>
-      <StyledSketchPicker className="mb-3" color={value} onChange={e => onChange && onChange(e.hex)} />
+      {visible && <StyledSketchPicker className="mb-3" color={value} onChange={e => onChange && onChange(e.hex)} />}
     </>
   )
 }
