a2bf15092e4934bcba7f7f6067876acab6e66feb
ponchimeow <trancetoradora@gmail.com>
2021-08-02 16:16:26 +0800

    [REFACTOR] remove LayoutBlock, replace CraftBoxModelBlock with CraftBoxModelInput

--- src/components/craft/CraftLayout.tsx
+++ src/components/craft/CraftLayout.tsx
@@ -1,33 +1,47 @@
 import { useNode, UserComponent } from '@craftjs/core'
-import { Button, Form } from 'antd'
+import { Button, Collapse, Form, Input, Select } from 'antd'
 import { useForm } from 'antd/lib/form/Form'
+import { replace } from 'ramda'
 import React from 'react'
 import { useIntl } from 'react-intl'
 import styled from 'styled-components'
 import { commonMessages, craftPageMessages } from '../../helpers/translation'
 import { CraftLayoutProps } from '../../types/craft'
-import { StyledSettingButtonWrapper } from '../admin'
+import {
+  AdminHeaderTitle,
+  StyledCollapsePanel,
+  StyledFullWidthSelect,
+  StyledInputNumber,
+  StyledSettingButtonWrapper,
+} from '../admin'
 import { BREAK_POINT } from '../common/Responsive'
-import CraftLayoutBlock from './CraftLayoutBlock'
+import CraftBoxModelInput, { formatBoxModelValue } from './CraftBoxModelInput'
 
 const StyledContainer = styled.div<{ mobile: CraftLayoutProps; desktop: CraftLayoutProps }>`
-  padding: ${props => `${props.mobile.padding}px`};
-
+  padding: ${props =>
+    `${props.mobile.padding.pt}px ${props.mobile.padding.pr}px ${props.mobile.padding.pb}px ${props.mobile.padding.pl}px`};
   @media (min-width: ${BREAK_POINT}px) {
-    padding: ${props => `${props.desktop.padding}px`};
+    padding: ${props =>
+      `${props.desktop.padding.pt}px ${props.desktop.padding.pr}px ${props.desktop.padding.pb}px ${props.desktop.padding.pl}px`};
   }
 `
 
 type FieldProps = {
-  desktop: CraftLayoutProps
-  mobile: CraftLayoutProps
+  desktopPadding: string
+  desktopColumnAmount: number
+  desktopColumnRatio: number[]
+  desktopDisplayAmount: number
+  mobilePadding: string
+  mobileColumnAmount: number
+  mobileColumnRatio: number[]
+  mobileDisplayAmount: number
 }
 
 const CraftLayout: UserComponent<{
-  mobile: CraftLayoutProps
   desktop: CraftLayoutProps
+  mobile: CraftLayoutProps
   setActiveKey: React.Dispatch<React.SetStateAction<string>>
-}> = ({ mobile, desktop, setActiveKey }) => {
+}> = ({ desktop, mobile, setActiveKey }) => {
   const {
     connectors: { connect, drag },
   } = useNode()
@@ -35,8 +49,8 @@ const CraftLayout: UserComponent<{
   return (
     <StyledContainer
       ref={ref => ref && connect(drag(ref))}
-      mobile={mobile}
       desktop={desktop}
+      mobile={mobile}
       style={{ cursor: 'pointer' }}
       onClick={() => setActiveKey('settings')}
     >
@@ -54,23 +68,36 @@ const LayoutSettings: React.VFC = () => {
     props,
     selected,
   } = useNode(node => ({
-    props: node.data.props as FieldProps,
+    props: node.data.props as { mobile: CraftLayoutProps; desktop: CraftLayoutProps },
     selected: node.events.selected,
   }))
 
-  const handleSubmit = (value: FieldProps) => {
-    setProp((props: FieldProps) => {
+  const handleSubmit = (values: FieldProps) => {
+    const desktopPadding = formatBoxModelValue(values.desktopPadding)
+    const mobilePadding = formatBoxModelValue(values.mobilePadding)
+
+    setProp(props => {
       props.desktop = {
-        padding: value.desktop.padding,
-        columnAmount: value.desktop.columnAmount,
-        columnRatio: value.desktop.columnRatio,
-        displayAmount: value.desktop.displayAmount,
+        padding: {
+          pt: desktopPadding?.[0] || '0',
+          pr: desktopPadding?.[1] || '0',
+          pb: desktopPadding?.[2] || '0',
+          pl: desktopPadding?.[3] || '0',
+        },
+        columnAmount: values.desktopColumnAmount,
+        columnRatio: values.desktopColumnRatio,
+        displayAmount: values.desktopDisplayAmount,
       }
       props.mobile = {
-        padding: value.mobile.padding,
-        columnAmount: value.mobile.columnAmount,
-        columnRatio: value.mobile.columnRatio,
-        displayAmount: value.mobile.displayAmount,
+        padding: {
+          pt: mobilePadding?.[0] || '0',
+          pr: mobilePadding?.[1] || '0',
+          pb: mobilePadding?.[2] || '0',
+          pl: mobilePadding?.[3] || '0',
+        },
+        columnAmount: values.mobileColumnAmount,
+        columnRatio: values.mobileColumnRatio,
+        displayAmount: values.mobileDisplayAmount,
       }
     })
   }
@@ -82,27 +109,111 @@ const LayoutSettings: React.VFC = () => {
       colon={false}
       requiredMark={false}
       initialValues={{
-        desktop: {
-          padding: props.desktop.padding || 0,
-          columnAmount: props.desktop.columnAmount || 3,
-          columnRatio: props.desktop.columnRatio || [3, 3, 3],
-          displayAmount: props.desktop.displayAmount || 3,
-        },
-        mobile: {
-          padding: props.mobile.padding || 0,
-          columnAmount: props.mobile.columnAmount || 3,
-          columnRatio: props.mobile.columnRatio || [3, 3, 3],
-          displayAmount: props.mobile.displayAmount || 3,
-        },
+        desktopPadding: `${props.desktop.padding?.pt || 0};${props.desktop.padding?.pr || 0};${
+          props.desktop.padding?.pb || 0
+        };${props.desktop.padding?.pl || 0}`,
+        desktopColumnAmount: props.desktop.columnAmount || 3,
+        desktopColumnRatio: replace(/,/g, ':', props.desktop.columnRatio.toString() || '3,3,3') || [3, 3, 3],
+        desktopDisplayAmount: props.desktop.displayAmount || 3,
+        mobilePadding: `${props.mobile.padding?.pt || 0};${props.mobile.padding?.pr || 0};${
+          props.mobile.padding?.pb || 0
+        };${props.mobile.padding?.pl || 0}`,
+        mobileColumnAmount: props.mobile.columnAmount || 3,
+        mobileColumnRatio: props.mobile.columnRatio || [3, 3, 3],
+        mobileDisplayAmount: props.mobile.displayAmount || 3,
       }}
       onFinish={handleSubmit}
     >
-      <Form.Item name="desktop">
-        <CraftLayoutBlock title={formatMessage(craftPageMessages.label.desktopLayoutComponent)} />
-      </Form.Item>
-      <Form.Item name="mobile">
-        <CraftLayoutBlock title={formatMessage(craftPageMessages.label.mobileLayoutComponent)} />
-      </Form.Item>
+      <Collapse
+        className="mt-2 p-0"
+        bordered={false}
+        expandIconPosition="right"
+        ghost
+        defaultActiveKey={['desktopLayoutComponent']}
+      >
+        <StyledCollapsePanel
+          key="desktopLayoutComponent"
+          header={<AdminHeaderTitle>{formatMessage(craftPageMessages.label.desktopLayoutComponent)}</AdminHeaderTitle>}
+        >
+          <Form.Item name="desktopPadding" label={formatMessage(craftPageMessages.label.boundary)}>
+            <CraftBoxModelInput />
+          </Form.Item>
+          <Form.Item name="desktopColumnAmount" label={formatMessage(craftPageMessages.label.columnAmount)}>
+            <StyledFullWidthSelect>
+              <Select.Option key="1" value="1">
+                1
+              </Select.Option>
+              <Select.Option key="2" value="2">
+                2
+              </Select.Option>
+              <Select.Option key="3" value="3">
+                3
+              </Select.Option>
+              <Select.Option key="4" value="4">
+                4
+              </Select.Option>
+              <Select.Option key="6" value="6">
+                6
+              </Select.Option>
+              <Select.Option key="12" value="12">
+                12
+              </Select.Option>
+            </StyledFullWidthSelect>
+          </Form.Item>
+          <Form.Item name="desktopColumnRatio" label={formatMessage(craftPageMessages.label.ratio)}>
+            <Input />
+          </Form.Item>
+          <Form.Item name="desktopDisplayAmount" label={formatMessage(craftPageMessages.label.displayAmount)}>
+            <StyledInputNumber />
+          </Form.Item>
+        </StyledCollapsePanel>
+      </Collapse>
+
+      <Collapse
+        className="mt-2 p-0"
+        bordered={false}
+        expandIconPosition="right"
+        ghost
+        defaultActiveKey={['mobileLayoutComponent']}
+      >
+        <StyledCollapsePanel
+          key="mobileLayoutComponent"
+          header={<AdminHeaderTitle>{formatMessage(craftPageMessages.label.mobileLayoutComponent)}</AdminHeaderTitle>}
+        >
+          <Form.Item name="mobilePadding" label={formatMessage(craftPageMessages.label.boundary)}>
+            <CraftBoxModelInput />
+          </Form.Item>
+          <Form.Item name="mobileColumnAmount" label={formatMessage(craftPageMessages.label.columnAmount)}>
+            <StyledFullWidthSelect>
+              <Select.Option key="1" value="1">
+                1
+              </Select.Option>
+              <Select.Option key="2" value="2">
+                2
+              </Select.Option>
+              <Select.Option key="3" value="3">
+                3
+              </Select.Option>
+              <Select.Option key="4" value="4">
+                4
+              </Select.Option>
+              <Select.Option key="6" value="6">
+                6
+              </Select.Option>
+              <Select.Option key="12" value="12">
+                12
+              </Select.Option>
+            </StyledFullWidthSelect>
+          </Form.Item>
+          <Form.Item name="mobileColumnRatio" label={formatMessage(craftPageMessages.label.ratio)}>
+            <Input />
+          </Form.Item>
+          <Form.Item name="mobileDisplayAmount" label={formatMessage(craftPageMessages.label.displayAmount)}>
+            <StyledInputNumber />
+          </Form.Item>
+        </StyledCollapsePanel>
+      </Collapse>
+
       {selected && (
         <StyledSettingButtonWrapper>
           <Button type="primary" block htmlType="submit">
