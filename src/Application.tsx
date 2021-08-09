7b7f686b8f89b76bef3dd097f17ff9fb48e7994b
ponchimeow <trancetoradora@gmail.com>
2021-08-09 11:46:39 +0800

    [FEATURE] add AppThemeContext

--- src/Application.tsx
+++ src/Application.tsx
@@ -4,16 +4,14 @@ import 'braft-editor/dist/index.css'
 import 'braft-editor/dist/output.css'
 import React from 'react'
 import { BrowserRouter, Route } from 'react-router-dom'
-import { ThemeProvider } from 'styled-components'
 import { QueryParamProvider } from 'use-query-params'
+import { AppThemeProvider } from './components/AppThemeContext'
 import { ApiProvider } from './contexts/ApiContext'
 import { AppProvider } from './contexts/AppContext'
 import { AuthProvider } from './contexts/AuthContext'
 import { CustomRendererProps, CustomRendererProvider } from './contexts/CustomRendererContext'
 import { LanguageProvider } from './contexts/LanguageContext'
 import Routes, { RouteProps } from './Routes'
-import './styles/default/index.scss'
-import theme from './theme/default.json'
 
 const Application: React.FC<{
   appId: string
@@ -27,13 +25,13 @@ const Application: React.FC<{
           <ApiProvider>
             <AppProvider appId={appId}>
               <LanguageProvider>
-                <ThemeProvider theme={theme}>
+                <AppThemeProvider>
                   <ConfigProvider locale={zhTW}>
                     <CustomRendererProvider renderer={customRender}>
                       <Routes extra={extraRouteProps} />
                     </CustomRendererProvider>
                   </ConfigProvider>
-                </ThemeProvider>
+                </AppThemeProvider>
               </LanguageProvider>
             </AppProvider>
           </ApiProvider>
