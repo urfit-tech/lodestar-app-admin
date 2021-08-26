a91ce9d5d880afc5ad6d70c7987b0d02d4e71039
ponchimeow <trancetoradora@gmail.com>
2021-08-26 17:24:11 +0800

    [FEATURE] add PermissionGroupPage basic

---
+++ src/pages/owner/PermissionGroupPage.tsx
@@ -0,0 +1,17 @@
+import { AdminHeader } from '../../components/admin'
+import { StyledLayoutContent } from '../../components/layout/DefaultLayout'
+
+const PermissionGroupPage: React.VFC = () => {
+  return (
+    <>
+      <AdminHeader></AdminHeader>
+      <StyledLayoutContent variant="gray"></StyledLayoutContent>
+    </>
+  )
+}
+
+const PermissionGroupCard: React.VFC = () => {
+  return <></>
+}
+
+export default PermissionGroupPage
