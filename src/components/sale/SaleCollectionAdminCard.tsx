bf36f3b422d5d7e111e46d08108b68a1d34d9132
emily40830 <emily40830@gmail.com>
2021-10-26 10:47:26 +0800

    [REFACTOR] move useOrderLog to hooks

--- src/components/sale/SaleCollectionAdminCard.tsx
+++ src/components/sale/SaleCollectionAdminCard.tsx
@@ -1,18 +1,15 @@
 import { SearchOutlined } from '@ant-design/icons'
-import { useQuery } from '@apollo/react-hooks'
 import { Button, Divider, Input, Table, Tooltip, Typography } from 'antd'
 import { ColumnProps } from 'antd/lib/table'
-import gql from 'graphql-tag'
 import { useApp } from 'lodestar-app-element/src/contexts/AppContext'
 import { useAuth } from 'lodestar-app-element/src/contexts/AuthContext'
 import moment from 'moment'
-import { prop, sum } from 'ramda'
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 import { useIntl } from 'react-intl'
 import styled, { css } from 'styled-components'
-import hasura from '../../hasura'
 import { currencyFormatter, dateFormatter, dateRangeFormatter, desktopViewMixin } from '../../helpers'
 import { commonMessages } from '../../helpers/translation'
+import { useOrderLog } from '../../hooks/order'
 import { OrderLogProps } from '../../types/general'
 import AdminCard from '../admin/AdminCard'
 import ProductTypeLabel from '../common/ProductTypeLabel'
@@ -63,7 +60,6 @@ const SaleCollectionAdminCard: React.VFC<{
   const [orderId, setOrderId] = useState<string | null>(null)
   const [memberNameAndEmail, setMemberNameAndEmail] = useState<string | null>(null)
   const [tmpOrderLogStatus, setTmpOrderLogStatus] = useState<{ [OrderId in string]?: string }>({})
-  const [totalOrderLogsCnt, setTotalOrderLogsCnt] = useState<number>(0)
 
   const { loadingOrderLogs, errorOrderLogs, orderLogs, totalCount, refetchOrderLogs, loadMoreOrderLogs } = useOrderLog({
     statuses,
@@ -71,12 +67,6 @@ const SaleCollectionAdminCard: React.VFC<{
     memberNameAndEmail,
     memberId,
   })
-
-  useEffect(() => {
-    if (totalCount > totalOrderLogsCnt) {
-      setTotalOrderLogsCnt(totalCount)
-    }
-  }, [totalCount])
 
   const getColumnSearchProps = ({
     onReset,
@@ -312,7 +302,7 @@ const SaleCollectionAdminCard: React.VFC<{
       <StyledContainer>
         <div className="d-flex justify-content-end">
           <Typography.Text type="secondary">
-            {formatMessage(commonMessages.text.totalCount, { count: `${totalOrderLogsCnt}` })}
+            {formatMessage(commonMessages.text.totalCount, { count: totalCount })}
           </Typography.Text>
         </div>
 
@@ -343,226 +333,5 @@ const SaleCollectionAdminCard: React.VFC<{
     </AdminCard>
   )
 }
-
-const useOrderLog = (filters?: {
-  statuses?: string[] | null
-  orderId?: string | null
-  memberNameAndEmail?: string | null
-  memberId?: string
-}) => {
-  const { permissions, currentMemberId } = useAuth()
-  const authStatus: 'Admin' | 'Creator' | 'None' = permissions.SALES_RECORDS_ADMIN
-    ? 'Admin'
-    : permissions.SALES_RECORDS_CREATOR
-    ? 'Creator'
-    : 'None'
-
-  const condition: hasura.GET_ORDERSVariables['condition'] = {
-    id: filters?.orderId ? { _ilike: `%${filters.orderId}%` } : undefined,
-    status: filters?.statuses ? { _in: filters.statuses } : undefined,
-    member:
-      authStatus !== 'None'
-        ? filters?.memberId || filters?.memberNameAndEmail
-          ? {
-              id: filters?.memberId ? { _like: `%${filters.memberId}%` } : undefined,
-              _or: filters?.memberNameAndEmail
-                ? [
-                    { name: { _ilike: `%${filters.memberNameAndEmail}%` } },
-                    { username: { _ilike: `%${filters.memberNameAndEmail}%` } },
-                    { email: { _ilike: `%${filters.memberNameAndEmail}%` } },
-                  ]
-                : undefined,
-            }
-          : undefined
-        : {
-            id: { _eq: currentMemberId },
-          },
-    order_products:
-      authStatus === 'Creator'
-        ? {
-            product: {
-              product_owner: {
-                member_id: { _eq: currentMemberId },
-              },
-            },
-          }
-        : undefined,
-  }
-
-  const { data: allOrderLogs } = useQuery<hasura.GET_ALL_ORDER_LOG, hasura.GET_ALL_ORDER_LOGVariables>(
-    gql`
-      query GET_ALL_ORDER_LOG($allcondition: order_log_bool_exp) {
-        order_log_aggregate(where: $allcondition) {
-          aggregate {
-            count
-          }
-        }
-      }
-    `,
-    {
-      variables: {
-        allcondition: {
-          member_id: authStatus === 'None' ? { _eq: currentMemberId } : undefined,
-          order_products:
-            authStatus === 'Creator'
-              ? {
-                  product: {
-                    product_owner: {
-                      member_id: { _eq: currentMemberId },
-                    },
-                  },
-                }
-              : undefined,
-        },
-      },
-    },
-  )
-
-  const totalCount = allOrderLogs?.order_log_aggregate.aggregate?.count || 0
-
-  const { loading, error, data, refetch, fetchMore } = useQuery<hasura.GET_ORDERS, hasura.GET_ORDERSVariables>(
-    GET_ORDERS,
-    {
-      variables: {
-        condition,
-        limit: 20,
-      },
-      context: {
-        important: true,
-      },
-    },
-  )
-  const loadMoreOrderLogs =
-    (data?.order_log_aggregate.aggregate?.count || 0) > 20
-      ? () => {
-          const lastOrderLog = data?.order_log[data.order_log.length - 1]
-          return fetchMore({
-            variables: {
-              condition: {
-                _and: [
-                  condition,
-                  {
-                    created_at: {
-                      _lte: lastOrderLog?.created_at,
-                    },
-                    id: {
-                      _nin: data?.order_log.filter(v => v.created_at === lastOrderLog?.created_at).map(v => v.id) || [],
-                    },
-                  },
-                ],
-              },
-              limit: 20,
-            },
-            updateQuery: (prev, { fetchMoreResult }) => {
-              if (!fetchMoreResult) {
-                return prev
-              }
-              return {
-                order_log_aggregate: fetchMoreResult.order_log_aggregate,
-                order_log: [...prev.order_log, ...fetchMoreResult.order_log],
-              }
-            },
-          })
-        }
-      : undefined
-
-  const orderLogs: OrderLogProps[] =
-    data?.order_log.map(v => ({
-      id: v.id,
-      createdAt: v.created_at,
-      status: v.status || '',
-      shipping: v.shipping,
-      name: v.member.name,
-      email: v.member.email,
-
-      orderProducts: v.order_products.map(w => ({
-        id: w.id,
-        name: w.name,
-        price: w.price,
-        startedAt: w.started_at && new Date(w.started_at),
-        endedAt: w.ended_at && new Date(w.ended_at),
-        product: w.product,
-        quantity: w.options?.quantity,
-        options: w.options,
-      })),
-
-      orderDiscounts: v.order_discounts.map(w => ({
-        id: w.id,
-        name: w.name,
-        description: w.description,
-        price: w.price,
-      })),
-
-      totalPrice: Math.max(
-        sum(v.order_products.map(prop('price'))) - sum(v.order_discounts.map(prop('price'))) + (v.shipping?.fee || 0),
-        0,
-      ),
-
-      expiredAt: v.expired_at,
-      paymentMethod: v.payment_logs[0]?.gateway,
-      orderExecutors: v.order_executors.map(w => w.member.name),
-    })) || []
-
-  return {
-    totalCount,
-    loadingOrderLogs: loading,
-    errorOrderLogs: error,
-    orderLogs,
-    refetchOrderLogs: refetch,
-    loadMoreOrderLogs,
-  }
-}
-
-const GET_ORDERS = gql`
-  query GET_ORDERS($condition: order_log_bool_exp, $limit: Int) {
-    order_log_aggregate(where: $condition) {
-      aggregate {
-        count
-      }
-    }
-
-    order_log(where: $condition, order_by: { created_at: desc }, limit: $limit) {
-      id
-      created_at
-      status
-      shipping
-      expired_at
-      member {
-        name
-        email
-      }
-
-      payment_logs(order_by: { created_at: desc }, limit: 1) {
-        gateway
-      }
-
-      order_products {
-        id
-        name
-        price
-        started_at
-        ended_at
-        product {
-          id
-          type
-        }
-        options
-      }
-
-      order_discounts {
-        id
-        name
-        description
-        price
-      }
-
-      order_executors {
-        member {
-          name
-        }
-      }
-    }
-  }
-`
 
 export default SaleCollectionAdminCard
