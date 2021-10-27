240f3700a76f6f732a32a5ae139f68ff0f8c5e35
徐愷 <kkshyu.tw@gmail.com>
2021-10-27 02:26:18 -0700

    [BUGFIX] remove unused image part

--- src/components/craft/settings/ImageSettings.tsx
+++ src/components/craft/settings/ImageSettings.tsx
@@ -1,30 +1,19 @@
 import Form from 'antd/lib/form/'
 import { useForm } from 'antd/lib/form/Form'
 import { ImageProps } from 'lodestar-app-element/src/components/common/Image'
-import { useApp } from 'lodestar-app-element/src/contexts/AppContext'
-import { useAuth } from 'lodestar-app-element/src/contexts/AuthContext'
-import React, { useState } from 'react'
-import { useIntl } from 'react-intl'
+import React from 'react'
 import { CSSObject } from 'styled-components'
-import { v4 as uuid } from 'uuid'
-import { handleError, uploadFile } from '../../../helpers/index'
-import { craftPageMessages } from '../../../helpers/translation'
-import ImageUploader from '../../common/ImageUploader'
-import BoxModelInput from '../inputs/BoxModelInput'
+import EmptyCover from '../../../images/default/empty-cover.png'
+import CustomStyleInput from '../inputs/CustomStyleInput'
 import { CraftSettings } from './CraftSettings'
 
-type FieldProps = {
+type FieldValues = {
   url: string
-  spaceStyle: CSSObject
+  customStyle: CSSObject
 }
 
 const ImageSettings: CraftSettings<ImageProps> = ({ props, onPropsChange }) => {
-  const { formatMessage } = useIntl()
-  const [form] = useForm<FieldProps>()
-  const { authToken } = useAuth()
-  const { id: appId } = useApp()
-  const [loading, setLoading] = useState(false)
-  const [imageFile, setImageFile] = useState<File | null>(null)
+  const [form] = useForm<FieldValues>()
 
   const handleChange = () => {
     form
@@ -32,28 +21,15 @@ const ImageSettings: CraftSettings<ImageProps> = ({ props, onPropsChange }) => {
       .then(values => {
         onPropsChange?.({
           src: values.url,
-          customStyle: {
-            ...props.customStyle,
-            ...values.spaceStyle,
-          },
+          customStyle: values.customStyle,
         })
       })
       .catch(() => {})
   }
 
-  const handleImageUpload = (file?: File) => {
-    if (file) {
-      setLoading(true)
-      const uniqImageId = uuid()
-      uploadFile(`images/${appId}/craft/${uniqImageId}`, file, authToken)
-        .then(() => {
-          onPropsChange?.({
-            src: `https://${process.env.REACT_APP_S3_BUCKET}/images/${appId}/craft/${uniqImageId}`,
-          })
-        })
-        .catch(handleError)
-        .finally(() => setLoading(false))
-    }
+  const initialValues: FieldValues = {
+    url: props.src || EmptyCover,
+    customStyle: props.customStyle || {},
   }
 
   return (
@@ -62,42 +38,11 @@ const ImageSettings: CraftSettings<ImageProps> = ({ props, onPropsChange }) => {
       layout="vertical"
       colon={false}
       requiredMark={false}
-      initialValues={props}
+      initialValues={initialValues}
       onChange={handleChange}
     >
-      <Form.Item name="imageFile">
-        <ImageUploader
-          uploading={loading}
-          file={imageFile}
-          initialCoverUrl={props.src}
-          onChange={file => handleImageUpload(file)}
-        />
-      </Form.Item>
-      <Form.Item
-        name="desktopMargin"
-        label={formatMessage(craftPageMessages.label.margin)}
-        rules={[
-          {
-            required: true,
-            pattern: /^\d+;\d+;\d+;\d+$/,
-            message: formatMessage(craftPageMessages.text.boxModelInputWarning),
-          },
-        ]}
-      >
-        <BoxModelInput onChange={handleChange} />
-      </Form.Item>
-      <Form.Item
-        name="desktopPadding"
-        label={formatMessage(craftPageMessages.label.padding)}
-        rules={[
-          {
-            required: true,
-            pattern: /^\d+;\d+;\d+;\d+$/,
-            message: formatMessage(craftPageMessages.text.boxModelInputWarning),
-          },
-        ]}
-      >
-        <BoxModelInput onChange={handleChange} />
+      <Form.Item name="customStyle">
+        <CustomStyleInput space border background />
       </Form.Item>
     </Form>
   )
