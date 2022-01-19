b2bfc2c0e132d5d2cef8146eb9fac62499ef0381
sunnylochun <69782240+sunnylochun@users.noreply.github.com>
2022-01-19 17:09:07 +0800

    Merge pull request #125 from urfit-tech/feature/collection-name
    [FEATURE] add collection name

--- src/components/common/LayoutInput.tsx
+++ src/components/common/LayoutInput.tsx
@@ -1,6 +1,7 @@
 import { Form, InputNumber } from 'antd'
 import { CollectionLayout } from 'lodestar-app-element/src/components/collections/Collection'
 import { defineMessages, useIntl } from 'react-intl'
+import { CraftSettingLabel } from '../../pages/craft/CraftPageAdminPage/CraftSettingsPanel'
 
 const labelMessages = defineMessages({
   columns: { id: 'common.ui.columns', defaultMessage: '欄數' },
@@ -16,13 +17,13 @@ const LayoutInput: React.FC<LayoutInputProps> = ({ value, onChange }) => {
   const { formatMessage } = useIntl()
   return (
     <>
-      <Form.Item label={formatMessage(labelMessages.columns)}>
+      <Form.Item label={<CraftSettingLabel>{formatMessage(labelMessages.columns)}</CraftSettingLabel>}>
         <InputNumber value={Number(value?.columns)} onChange={v => onChange?.({ ...value, columns: Number(v) })} />
       </Form.Item>
-      <Form.Item label={formatMessage(labelMessages.gutter)}>
+      <Form.Item label={<CraftSettingLabel>{formatMessage(labelMessages.gutter)}</CraftSettingLabel>}>
         <InputNumber value={Number(value?.gutter)} onChange={v => onChange?.({ ...value, gutter: Number(v) })} />
       </Form.Item>
-      <Form.Item label={formatMessage(labelMessages.gap)}>
+      <Form.Item label={<CraftSettingLabel>{formatMessage(labelMessages.gap)}</CraftSettingLabel>}>
         <InputNumber value={Number(value?.gap)} onChange={v => onChange?.({ ...value, gap: Number(v) })} />
       </Form.Item>
     </>
