ff719d73515b5071a8edd5b6bae733a04c7f974d
ponchimeow <trancetoradora@gmail.com>
2022-03-18 19:49:38 +0800

    [FEATURE] change permission logic

--- src/pages/PracticeCollectionAdminPage.tsx
+++ src/pages/PracticeCollectionAdminPage.tsx
@@ -88,7 +88,7 @@ const AllPracticeCollectionBlock: React.FC<{
   filters?: PracticeFiltersProps
 }> = ({ selectedStatus, filters }) => {
   const { formatMessage } = useIntl()
-  const { currentMemberId, currentUserRole } = useAuth()
+  const { currentMemberId, currentUserRole, permissions } = useAuth()
 
   let unreviewed: boolean | undefined
   switch (selectedStatus) {
@@ -103,7 +103,7 @@ const AllPracticeCollectionBlock: React.FC<{
   const { loadingPractice, errorPractice, practices, refetchPractice } = usePracticePreviewCollection({
     ...filters,
     unreviewed,
-    programRoleMemberId: currentUserRole !== 'app-owner' ? currentMemberId : undefined,
+    programRoleMemberId: currentUserRole === 'app-owner' || permissions['PRACTICE_ADMIN'] ? undefined : currentMemberId,
   })
 
   if (loadingPractice) {
