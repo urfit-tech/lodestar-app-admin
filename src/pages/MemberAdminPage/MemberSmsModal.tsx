0a1f83f80ace00e4a909513793ffe1ee1b842a1e
Hedgehog Chen <jhihruei@users.noreply.github.com>
2022-09-26 02:33:42 +0800

    [UPDATE] Adjust SMS modal related text

--- src/pages/MemberAdminPage/MemberSmsModal.tsx
+++ src/pages/MemberAdminPage/MemberSmsModal.tsx
@@ -5,13 +5,16 @@ import axios from 'axios'
 import { useAuth } from 'lodestar-app-element/src/contexts/AuthContext'
 import { Moment } from 'moment'
 import { useState } from 'react'
+import { useIntl } from 'react-intl'
 import AdminModal from '../../components/admin/AdminModal'
+import { memberMessages } from '../../helpers/translation'
 
 const MemberSmsModel: React.VFC<{ memberId: string; phone: string, name: string }> = ({ memberId, phone, name }) => {
   const [isSending, setIsSending] = useState(false)
   const [content, setContent] = useState('')
   const [sentAt, setSentAt] = useState<Moment | null>(null)
   const { authToken } = useAuth()
+  const { formatMessage } = useIntl()
 
   return (
     <AdminModal
@@ -35,9 +38,11 @@ const MemberSmsModel: React.VFC<{ memberId: string; phone: string, name: string 
           )
           .then(({ data: { code, error, result } }) => {
             if (code === 'SUCCESS') {
-              message.success('sent successfully')
+              message.success(formatMessage(memberMessages.text.smsSucceed))
             } else {
-              message.error(`sent faild: ${error}`)
+              message.error(formatMessage(memberMessages.text.smsFailed, {
+                errorMessage: error
+              }))
             }
           })
           .finally(() => {
@@ -53,7 +58,7 @@ const MemberSmsModel: React.VFC<{ memberId: string; phone: string, name: string 
       <div className="mb-3">{`${name} / ${phone}`}</div>
       <TextArea
         className="mb-3"
-        placeholder="content"
+        placeholder={formatMessage(memberMessages.placeholder.smsContent)}
         required
         value={content}
         onChange={e => setContent(e.target.value)}
@@ -61,7 +66,7 @@ const MemberSmsModel: React.VFC<{ memberId: string; phone: string, name: string 
       <DatePicker
         style={{ width: '100%' }}
         showTime
-        placeholder="scheduled date to sent (optional)"
+        placeholder={formatMessage(memberMessages.placeholder.smsSchedule)}
         value={sentAt}
         onChange={date => setSentAt(date)}
       />
