c26f1594f223cc3e5df037b14e7076ce66bd3f47
Sam Ho <twhltvproxy@gmail.com>
2022-09-28 16:56:09 +0800

    [BUGFIX] modify productGiftPlan data
    # Conflicts:
    #	package.json
    #	yarn.lock

--- src/types/giftPlan.ts
+++ src/types/giftPlan.ts
@@ -1,22 +1,27 @@
+export type ProductGiftPlan = {
+  id: string
+  giftPlan: GiftPlan
+  startedAt: string
+  endedAt: string
+}
+
 export type GiftPlan = {
   id: string
-  title: string
-  createdAt: string
-  giftIdList: string[]
-  giftPlanProductIdList: string[]
+  title?: string
+  gift: Gift
 }
 
 export type Gift = {
   id: string
-  title: string
-  coverUrl: string | null
+  title?: string
+  coverUrl?: string | null
+  price?: number
+  currencyId?: string
   isDeliverable?: boolean
 }
 
-export type ProductGiftPlan = {
-  productGiftPlanId: string,
-  giftPlanId: string,
-  giftPlanName?: string,
-  startedAt: string,
-  endedAt: string,
+export type GiftPlanCollectionProps = Pick<GiftPlan, 'id' | 'title'> & {
+  createdAt: string
+  giftIdList: string[]
+  giftPlanProductIdList: string[]
 }
