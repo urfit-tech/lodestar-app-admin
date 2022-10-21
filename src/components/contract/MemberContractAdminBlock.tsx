1c15feefdbbffff67758634f6b9363b73d79f69b
logan <clothe09986@gmail.com>
2022-10-21 13:42:17 +0800

    [BUGFIX] fix undefind value

--- src/components/contract/MemberContractAdminBlock.tsx
+++ src/components/contract/MemberContractAdminBlock.tsx
@@ -142,14 +142,14 @@ const MemberContractAdminBlock: React.FC<{
                   })}
                 </div>
               )}
-              {contract.options.approvedAt && (
+              {contract.options?.approvedAt && (
                 <div>
                   {formatMessage(memberMessages.text.approvedAt, {
                     time: moment(contract.options.approvedAt).format('YYYY-MM-DD HH:mm:ss'),
                   })}
                 </div>
               )}
-              {contract.options.loanCanceledAt && (
+              {contract.options?.loanCanceledAt && (
                 <div>
                   {formatMessage(memberMessages.text.loanCanceledAt, {
                     time: moment(contract.options.loanCanceledAt).format('YYYY-MM-DD HH:mm:ss'),
@@ -163,7 +163,7 @@ const MemberContractAdminBlock: React.FC<{
                   })}
                 </div>
               )}
-              {contract.options.refundAppliedAt && (
+              {contract.options?.refundAppliedAt && (
                 <div>
                   {formatMessage(memberMessages.text.refundAppliedAt, {
                     time: moment(contract.options.refundAppliedAt).format('YYYY-MM-DD HH:mm:ss'),
