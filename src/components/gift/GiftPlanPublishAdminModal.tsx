c596fcaa580633a8624c8f07fff847ba2410e5e9
Sam Ho <twhltvproxy@gmail.com>
2022-10-21 18:25:59 +0800

    [BUGFIX] remove console.log

--- src/components/gift/GiftPlanPublishAdminModal.tsx
+++ src/components/gift/GiftPlanPublishAdminModal.tsx
@@ -14,7 +14,6 @@ const GiftPlanPublishAdminModal: React.VFC<{
 
   const handlePublish = (id: string, setVisible: (visible: boolean) => void) => {
     setLoading(true)
-    console.log(id)
     setLoading(false)
     setVisible(false)
   }
