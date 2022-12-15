62aedd5c533f5a0830944e85d46c4d4f60089011
Sam Ho <twhltvproxy@gmail.com>
2022-12-15 15:24:50 +0800

    [BUGFIX] fix app setting can't save number field

--- src/pages/AppSettingAdminPage.tsx
+++ src/pages/AppSettingAdminPage.tsx
@@ -104,7 +104,7 @@ const AppSettingCard: React.FC<
             .map(key => ({
               app_id: appId,
               key,
-              value: trim(values[key as keyof FieldProps]),
+              value: trim(String(values[key as keyof FieldProps])),
             })),
         },
       })
