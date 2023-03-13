7683734754ea42116783ea96912dda405ddb0be5
Torai Lin <torailin55@gmail.com>
2023-03-13 15:34:11 +0800

    fix: check duplicate channel sku

--- src/components/common/ProductSkuModal.tsx
+++ src/components/common/ProductSkuModal.tsx
@@ -1,11 +1,14 @@
+import Icon from '@ant-design/icons'
 import { useMutation } from '@apollo/react-hooks'
 import { Button, Checkbox, Form, Input, message } from 'antd'
 import { useForm } from 'antd/lib/form/Form'
 import gql from 'graphql-tag'
 import { useApp } from 'lodestar-app-element/src/contexts/AppContext'
+import { ExclamationCircleIcon } from 'lodestar-app-element/src/images'
 import { flatten, mergeAll } from 'ramda'
 import React, { useState } from 'react'
 import { useIntl } from 'react-intl'
+import styled from 'styled-components'
 import hasura from '../../hasura'
 import { handleError } from '../../helpers'
 import { commonMessages } from '../../helpers/translation'
@@ -13,9 +16,29 @@ import { useProductChannelInfo, useUpsertProductChannel } from '../../hooks/chan
 import { useProductSku } from '../../hooks/data'
 import AdminModal, { AdminModalProps } from '../admin/AdminModal'
 
+const StyledIcon = styled(Icon)`
+  vertical-align: baseline;
+  font-size: 20px;
+`
+
+const StyledCheckbox = styled(Checkbox)`
+  display: block;
+  margin-left: 0px;
+`
+
 type FieldProps = {
   sku?: string
 } & { [key: string]: string }
+
+type SkuError =
+  | {
+      message: string
+      productId: string
+      link: string
+    }
+  | {
+      message: string
+    }
 
 const ProductSkuModal: React.FC<
   Omit<AdminModalProps, 'renderTrigger'> & {
@@ -34,6 +57,7 @@ const ProductSkuModal: React.FC<
   const { id: appId } = useApp()
   const [form] = useForm<FieldProps>()
   const [loading, setLoading] = useState(false)
+  const [skuError, setSkuError] = useState<SkuError | null>(null)
   const { loadingProduct, product, refetchProduct } = useProductSku(productId)
   const [updateProductSku] = useMutation<hasura.UPDATE_PRODUCT_SKU, hasura.UPDATE_PRODUCT_SKUVariables>(
     UPDATE_PRODUCT_SKU,
@@ -62,6 +86,46 @@ const ProductSkuModal: React.FC<
       .validateFields()
       .then(() => {
         setLoading(true)
+        const formValues = form.getFieldsValue()
+        const checkChannelSkuDuplicate = () => {
+          const channelSkuMapList: [string, string][] = []
+          Object.entries(formValues).forEach(v => v[0] !== 'sku' && channelSkuMapList.push(v))
+
+          const validationObject: Record<string, Array<string>> = {}
+          for (const channelSkuMap of channelSkuMapList) {
+            const [channelId, channelSku] = channelSkuMap
+            validationObject[channelSku]
+              ? validationObject[channelSku].push(channelId)
+              : (validationObject[channelSku] = [channelId])
+          }
+
+          let duplicatedChannelIds: string[] = []
+          for (const key in validationObject) {
+            const channelIds = validationObject[key]
+            if (channelIds.length > 1) {
+              duplicatedChannelIds = duplicatedChannelIds.concat(channelIds)
+            }
+          }
+
+          return duplicatedChannelIds
+        }
+
+        const duplicatedChannelIds = checkChannelSkuDuplicate()
+        //FIXME: set input border color
+        // duplicatedChannelIds.forEach(channelId => {
+        //   form.getFieldInstance(channelId).style.borderColor = '#ff7d62'
+        // })
+
+        if (duplicatedChannelIds.length > 0) {
+          setSkuError({ message: '通路料號重複，請重新設定' })
+          return Promise.reject(duplicatedChannelIds)
+        }
+        return formValues
+      })
+      .then(formValues => {
+        // FIXME: check sku exists
+      })
+      .then(() => {
         const { sku } = form.getFieldsValue()
         updateProductSku({
           variables: {
@@ -76,7 +140,7 @@ const ProductSkuModal: React.FC<
               product_id: productId,
               app_id: appId,
               channel_id: id,
-              channel_sku: formValues[id] || null,
+              channel_sku: formValues[id].trim() || null,
             }))
             return upsertProductChannel({
               variables: {
@@ -91,9 +155,9 @@ const ProductSkuModal: React.FC<
             refetchProductChannelInfo()
           })
           .catch(handleError)
-          .finally(() => setLoading(false))
       })
       .catch(() => {})
+      .finally(() => setLoading(false))
   }
 
   return (
@@ -148,6 +212,18 @@ const ProductSkuModal: React.FC<
           ))}
         </Form.Item>
       </Form>
+      {skuError && (
+        <div>
+          <StyledIcon component={() => <ExclamationCircleIcon />} className="mr-2" />
+          <span style={{ color: '#ff7d62' }}>
+            {skuError.message}
+            {/* FIXME: link to product admin page */}
+            {/* <Link to={'skuError.link'} color="#ff7d62">
+              {skuError.message}
+            </Link> */}
+          </span>
+        </div>
+      )}
     </AdminModal>
   )
 }
@@ -162,9 +238,9 @@ const ChannelCheckbox: React.VFC<{ channelId: string; name: string; channelSku: 
 
   return (
     <>
-      <Checkbox className="mb-1" defaultChecked={isChecked} onChange={v => setIsChecked(v.target.checked)}>
+      <StyledCheckbox className="mb-1" defaultChecked={isChecked} onChange={v => setIsChecked(v.target.checked)}>
         {name}
-      </Checkbox>
+      </StyledCheckbox>
       {isChecked && (
         <Form.Item name={channelId}>
           <Input placeholder={formatMessage(commonMessages.placeholder.enterChannelSku)} />
