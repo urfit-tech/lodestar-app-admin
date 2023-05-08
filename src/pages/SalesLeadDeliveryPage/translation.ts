9ef5b8d898ed4b7aad749560523f158890813d5b
Hanz <e102777tw@gmail.com>
2023-05-08 12:37:04 +0800

    feature: sale lead filter exact match

--- src/pages/SalesLeadDeliveryPage/translation.ts
+++ src/pages/SalesLeadDeliveryPage/translation.ts
@@ -28,5 +28,6 @@ export const salesLeadDeliveryPageMessages = {
     deliveredCount: { id: 'page.salesLeadDeliveryPage.deliveredCount', defaultMessage: '已派發 {count} 筆名單' },
     deliveringMessage: { id: 'page.salesLeadDeliveryPage.deliveringMessage', defaultMessage: '正在派發名單中，請稍等' },
     deliverAgain: { id: 'page.salesLeadDeliveryPage.deliverAgain', defaultMessage: '再次派發' },
+    exactMatch: { id: 'page.salesLeadDeliveryPage.exactMatch', defaultMessage: '完全匹配' },
   }),
 }
