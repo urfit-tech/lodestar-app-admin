baac509d857dbf0c1e97d3c2bc2fa76c3cfc5c0e
Clooooode <jackey8616@gmail.com>
2023-06-08 10:42:44 +0800

    Revert "Merge pull request #316 from urfit-tech/feature/export-and-import-member"
    This reverts commit 01cf4e65f7e8bdadf7a41f2b336b93fd8e0f979a, reversing
    changes made to a157854bf1a2299b38d1ca678b776a248ab0d459.
    Also update element to v1.48.0.

--- src/components/member/MemberImportModal.tsx
+++ src/components/member/MemberImportModal.tsx
@@ -1,12 +1,9 @@
 import { ImportOutlined } from '@ant-design/icons'
 import { Alert, Button, Form, message, Upload } from 'antd'
 import { useForm } from 'antd/lib/form/Form'
-import dayjs from 'dayjs'
-import { useApp } from 'lodestar-app-element/src/contexts/AppContext'
 import { useAuth } from 'lodestar-app-element/src/contexts/AuthContext'
 import React, { useState } from 'react'
 import { useIntl } from 'react-intl'
-import { uploadFileV2 } from '../../helpers'
 import { commonMessages } from '../../helpers/translation'
 import AdminModal from '../admin/AdminModal'
 
@@ -14,15 +11,18 @@ const MemberImportModal: React.FC<{
   onRefetch?: () => void
 }> = ({ onRefetch }) => {
   const { authToken } = useAuth()
-  const { id: appId } = useApp()
   const { formatMessage } = useIntl()
   const [form] = useForm()
   const [responseList, setResponseList] = useState<
     {
-      status: number
-      statusText: string
-      data: string | null
-      name: string | null
+      code: string
+      message: string
+      result: {
+        totalLeads: number
+        existedLeads: number
+        mergedLeads: number
+        newLeads: number
+      } | null
     }[]
   >([])
 
@@ -42,22 +42,13 @@ const MemberImportModal: React.FC<{
           <Upload
             multiple
             name="memberList"
-            customRequest={({ file, onSuccess, onProgress, onError }) => {
-              uploadFileV2(
-                `members_import_${dayjs(new Date()).format('YYYY-MM-DDTHH:mm:ssZ[Z]')}`,
-                file,
-                authToken,
-                appId,
-              )
-                .then(res => onSuccess(res, file))
-                .catch(error => onError(error))
-            }}
-            accept=".csv,.xlsx,.xls"
+            method="POST"
+            action={`${process.env.REACT_APP_API_BASE_ROOT}/sys/import-leads`}
+            headers={{ authorization: `Bearer ${authToken}` }}
+            accept=".csv"
             onChange={info => {
-              console.log(info)
               if (info.file.status === 'done') {
                 const response = info.file.response
-                response.name = info.file.name
                 setResponseList(state => [...state, response])
                 onRefetch?.()
               } else if (info.file.status === 'error') {
@@ -70,21 +61,24 @@ const MemberImportModal: React.FC<{
         </Form.Item>
       </Form>
       {responseList.map(response => {
-        switch (response.status) {
-          case 201:
+        switch (response.code) {
+          case 'SUCCESS':
             return (
               <Alert
-                message={`${response.name}上傳成功!`}
+                message="Import successfully!"
                 type="success"
                 description={
                   <div>
-                    <div>匯入結果將會以信件寄出</div>
+                    <div>Total: {response.result?.totalLeads}</div>
+                    <div>Existed: {response.result?.existedLeads}</div>
+                    <div>Merged: {response.result?.mergedLeads}</div>
+                    <div>New: {response.result?.newLeads}</div>
                   </div>
                 }
               />
             )
           default:
-            return <Alert message={`${response.name}上傳失敗!`} type="error" />
+            return <Alert message={response.message} type="error" />
         }
       })}
     </AdminModal>
