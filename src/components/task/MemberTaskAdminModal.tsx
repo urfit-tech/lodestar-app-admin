18df5e7279f27b967b708f8e4322deced388a42c
Hanz <e102777tw@gmail.com>
2023-06-12 05:14:51 +0800

    feat: refactor meeting logic

--- src/components/task/MemberTaskAdminModal.tsx
+++ src/components/task/MemberTaskAdminModal.tsx
@@ -11,7 +11,7 @@ import React, { useState } from 'react'
 import { useIntl } from 'react-intl'
 import styled from 'styled-components'
 import hasura from '../../hasura'
-import { handleError } from '../../helpers'
+import { createMeeting, deleteMeeting, handleError, updateMeeting } from '../../helpers'
 import { commonMessages, errorMessages, memberMessages } from '../../helpers/translation'
 import { ReactComponent as ExternalLinkIcon } from '../../images/icon/external-link-square.svg'
 import { MemberTaskProps } from '../../types/member'
@@ -19,8 +19,6 @@ import { MemberTaskTag } from '../admin'
 import AdminModal, { AdminModalProps } from '../admin/AdminModal'
 import CategorySelector from '../form/CategorySelector'
 import { AllMemberSelector } from '../form/MemberSelector'
-import axios from 'axios'
-import { useToast } from '@chakra-ui/toast'
 import { useApp } from 'lodestar-app-element/src/contexts/AppContext'
 
 const StyledLinkIconWrapper = styled.span`
@@ -63,79 +61,8 @@ const MemberTaskAdminModal: React.FC<
   const [insertTask] = useMutation<hasura.INSERT_TASK, hasura.INSERT_TASKVariables>(INSERT_TASK)
   const [deleteTask] = useMutation<hasura.DELETE_TASK, hasura.DELETE_TASKVariables>(DELETE_TASK)
   const [loading, setLoading] = useState(false)
-  const toast = useToast()
   const { id: appId } = useApp()
-  const deleteMeeting = async (meetId: string) => {
-    if (!meetId) return
-    const response = await axios.delete(`${process.env.REACT_APP_KOLABLE_SERVER_ENDPOINT}/kolable/meets/${meetId}`, {
-      headers: {
-        Authorization: `Bearer ${authToken}`,
-        'x-api-key': 'kolable',
-      },
-    })
-    return response.status
-  }
-  const updateMeeting = async (
-    meetId: string,
-    meetingMemberId: string | null,
-    meetingStartedAt: Moment | null,
-    meetingHours: number,
-  ) => {
-    if (!meetId) return
-    await axios.put(
-      `${process.env.REACT_APP_KOLABLE_SERVER_ENDPOINT}/kolable/meets/${meetId}`,
-      {
-        name: `${process.env.NODE_ENV === 'development' ? 'dev' : appId}-${meetingMemberId}`,
-        autoRecording: true,
-        service: 'zoom',
-        nbfAt: meetingStartedAt?.toDate(),
-        expAt: moment(meetingStartedAt).add(meetingHours, 'hours').toDate(),
-        startedAt: meetingStartedAt?.toDate(),
-        endedAt: moment(meetingStartedAt).add(meetingHours, 'hours').toDate(),
-      },
-      {
-        headers: {
-          Authorization: `Bearer ${authToken}`,
-          'x-api-key': 'kolable',
-        },
-      },
-    )
-  }
-  const createMeeting = async (
-    meetingMemberId: string | null,
-    meetingStartedAt: Moment | null,
-    meetingHours: number,
-  ) => {
-    try {
-      const response = await axios.post(
-        `${process.env.REACT_APP_KOLABLE_SERVER_ENDPOINT}/kolable/meets`,
-        {
-          name: `${process.env.NODE_ENV === 'development' ? 'dev' : appId}-${meetingMemberId}`,
-          autoRecording: true,
-          service: 'zoom',
-          nbfAt: meetingStartedAt?.toDate(),
-          expAt: moment(meetingStartedAt).add(meetingHours, 'hours').toDate(),
-          startedAt: meetingStartedAt?.toDate(),
-          endedAt: moment(meetingStartedAt).add(meetingHours, 'hours').toDate(),
-        },
-        {
-          headers: {
-            Authorization: `Bearer ${authToken}`,
-            'x-api-key': 'kolable',
-          },
-        },
-      )
-      return response.data.data.id
-    } catch {
-      toast({
-        title: '已達同時會議上限額度，請升級方案，將以 jitsi 開啟服務',
-        status: 'error',
-        duration: 3000,
-        position: 'top',
-      })
-      return null
-    }
-  }
+
   const handleSubmit = (onSuccess?: () => void) => {
     setLoading(true)
     form
@@ -143,20 +70,38 @@ const MemberTaskAdminModal: React.FC<
       .then(async () => {
         const values = form.getFieldsValue()
         let meetId = memberTask?.meet.id
+        // check if meeting time changed
         const isMeetingTimeChanged =
-          values.dueAt !== memberTask?.dueAt && values.meetingHours !== memberTask?.meetingHours
-        if (!values.hasMeeting && memberTask?.meet.id) {
-          // if hasMeeting is false and there is meet id =>soft delete meet
-          await deleteMeeting(memberTask?.meet.id)
-          meetId = null
-        }
-        if (values.hasMeeting && memberTask?.meet.id && isMeetingTimeChanged) {
-          // if hasMeeting is true and there is meet id => update meet
-          await updateMeeting(memberTask?.meet.id, values.memberId, values.dueAt, values.meetingHours)
-        }
+          values.dueAt?.format('YYYY-MM-DD HH:mm:ss') !== moment(memberTask?.dueAt).format('YYYY-MM-DD HH:mm:ss') ||
+          values.meetingHours !== memberTask?.meetingHours
+
+        // if hasMeeting is true and there is no meet id => create meet
         if (values.hasMeeting && !memberTask?.meet.id) {
-          // if hasMeeting is true and there is no meet id => create meet
-          meetId = await createMeeting(values.memberId, values.dueAt, values.meetingHours)
+          const createResult = await createMeeting(values.memberId, values.dueAt, values.meetingHours, appId, authToken)
+          meetId = createResult?.meetId
+          if (!createResult?.continueInsertTask) return
+        }
+        // if hasMeeting is true, meeting time change and there is meet id => update meet
+        if (values.hasMeeting && memberTask?.meet.id && isMeetingTimeChanged) {
+          const updateResult = await updateMeeting(
+            memberTask?.meet.id,
+            values.memberId,
+            values.dueAt,
+            values.meetingHours,
+            appId,
+            authToken,
+          )
+          meetId = updateResult?.meetId
+          if (!updateResult?.continueInsertTask) return
+          // if user want to use jitsi instead of zoom , delete meeting
+          if (updateResult?.continueInsertTask && !meetId) {
+            await deleteMeeting(memberTask?.meet.id, authToken)
+          }
+        }
+        // if hasMeeting is false and there is meet id => soft delete meet
+        if (!values.hasMeeting && memberTask?.meet.id) {
+          await deleteMeeting(memberTask?.meet.id, authToken)
+          meetId = null
         }
         const insertTaskData = {
           title: values.title || '',
@@ -215,7 +160,7 @@ const MemberTaskAdminModal: React.FC<
                     onClick={async () => {
                       setLoading(true)
                       await deleteTask({ variables: { taskId: memberTask.id } }).catch(handleError)
-                      await deleteMeeting(memberTask.meet.id).catch(handleError)
+                      await deleteMeeting(memberTask.meet.id, authToken).catch(handleError)
                       onRefetch?.()
                     }}
                   >
