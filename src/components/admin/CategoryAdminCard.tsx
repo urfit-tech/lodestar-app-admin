3e0cd5ed2142326c3e7bca9171e44db57e9046d2
Sam Ho <twhltvproxy@gmail.com>
2023-08-10 13:58:17 +0800

    fix: add clean member_task category in DELETE_CATEGORY mutation

--- src/components/admin/CategoryAdminCard.tsx
+++ src/components/admin/CategoryAdminCard.tsx
@@ -147,6 +147,11 @@ const DELETE_CATEGORY = gql`
     delete_voucher_category(where: { category_id: { _eq: $categoryId } }) {
       affected_rows
     }
+    update_member_task(where: { category_id: { _eq: $categoryId } }, _set: { category_id: null }) {
+      returning {
+        id
+      }
+    }
     delete_category(where: { id: { _eq: $categoryId } }) {
       affected_rows
     }
