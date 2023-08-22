da1f34790ab2b5080431c38c75b85335508f84ed
loischen68 <a8283626@gmail.com>
2023-08-22 13:04:15 +0800

    feat: add video flow control display

--- src/pages/MediaLibraryPage/MediaLibraryUsageCard.tsx
+++ src/pages/MediaLibraryPage/MediaLibraryUsageCard.tsx
@@ -5,13 +5,22 @@ import { useApp } from 'lodestar-app-element/src/contexts/AppContext'
 import moment from 'moment'
 import { RangeValue } from 'rc-picker/lib/interface'
 import React, { useState } from 'react'
+import { useIntl } from 'react-intl'
 import AdminCard from '../../components/admin/AdminCard'
-import { useAppUsage } from '../../hooks/data'
+import { useAppPlan, useAppUsage } from '../../hooks/data'
+import pageMessages from '../translation'
 
 const MediaLibraryUsageCard: React.VFC = () => {
+  const { formatMessage } = useIntl()
   const { settings } = useApp()
   const [dateRange, setDateRange] = useState<RangeValue<moment.Moment>>([moment().subtract(1, 'day'), moment()])
   const { ticks, totalVideoDuration, totalWatchedSeconds } = useAppUsage(dateRange)
+  const { appPlan } = useAppPlan()
+
+  const formatUnit = (unit: string, mapping: Record<string, string>): string => {
+    return mapping[unit] || unit
+  }
+
   const DemoDualAxes = () => {
     const data = ticks.map(tick => ({
       dateHour: tick.dateHour.format('MM/DD HH:00'),
@@ -66,8 +75,20 @@ const MediaLibraryUsageCard: React.VFC = () => {
             date > moment() || (settings['usage.startedAt'] ? date < moment(settings['usage.startedAt']) : false)
           }
         />
-        <div className="mr-3">Duration: {Math.round(totalVideoDuration / 60)}min(s)</div>
-        <div>Watched: {Math.round(totalWatchedSeconds / 60)}min(s)</div>
+        <div className="mr-3">
+          {formatMessage(pageMessages.MediaLibraryPage.maxVideoDuration)} : {Math.round(totalVideoDuration / 60)} /{' '}
+          {appPlan.options.maxVideoDuration}{' '}
+          {formatUnit(appPlan.options.maxVideoDurationUnit, {
+            minute: formatMessage(pageMessages.MediaLibraryPage.maxVideoDurationUnit),
+          })}
+        </div>
+        <div>
+          {formatMessage(pageMessages.MediaLibraryPage.maxVideoWatch)} : {Math.round(totalWatchedSeconds / 60)} /{' '}
+          {appPlan.options.maxVideoWatch}{' '}
+          {formatUnit(appPlan.options.maxVideoWatchUnit, {
+            minute: formatMessage(pageMessages.MediaLibraryPage.maxVideoWatchUnit),
+          })}
+        </div>
       </div>
 
       <DemoDualAxes />
