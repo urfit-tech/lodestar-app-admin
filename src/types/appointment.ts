af582bfd0114059a44bd031876339051e9bd069f
ponchimeow <trancetoradora@gmail.com>
2023-08-26 22:30:40 +0800

    feat: remove unused type

--- src/types/appointment.ts
+++ src/types/appointment.ts
@@ -24,27 +24,6 @@ export type AppointmentPlan = {
   defaultMeetSystem: string
 }
 
-export type AppointmentPlanAdminProps = {
-  id: string
-  title: string
-  phone: string
-  description: string | null
-  duration: number
-  listPrice: number
-  schedules: AppointmentSchedule[]
-  periods: AppointmentPeriod[]
-  isPublished: boolean | null
-  supportLocales: string[]
-  currencyId: string
-  creatorId: string
-  isPrivate: boolean
-  reservationAmount: number
-  reservationType: ReservationType | null
-  // capacity=-1 represents no limit
-  capacity: number
-  meetGenerationMethod: MeetGenerationMethod
-}
-
 export type AppointmentPeriod = {
   appointmentPlanId: string
   appointmentScheduleId: string
