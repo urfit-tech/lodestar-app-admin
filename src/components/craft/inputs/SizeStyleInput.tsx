508f6fb8d9ba52299357e83c521090ac1d743d10
ponchimeow <trancetoradora@gmail.com>
2024-03-05 13:47:26 +0800

    feat: img setting

--- src/components/craft/inputs/SizeStyleInput.tsx
+++ src/components/craft/inputs/SizeStyleInput.tsx
@@ -1,30 +1,35 @@
 import { Form, InputNumber, Select } from 'antd'
 import { useIntl } from 'react-intl'
 import { CSSObject } from 'styled-components'
+import { convertPxToUnit, convertToPx, extractNumber, extractSizeUnit } from '../../../helpers'
 import craftMessages from '../translation'
-
-export const extractNumber = (string?: string) =>
-  string?.match(/\d+/g)?.[0] ? Number(string.match(/\d+/g)?.[0]) : undefined
-
-export const extractSizeUnit = (string?: string) => string?.match(/px|%|rem|em|vw|vh/g)?.[0]
 
 export type SizeStyle = Pick<CSSObject, 'width' | 'height' | 'backgroundImage'>
 type imgProps = {
   width: number
   height: number
   aspectRatio: number
-  gcd: number
-  widthAspect: number
-  heightAspect: number
+  originalImage: {
+    width: string
+    height: string
+    ratio: number
+  }
 }
 type SizeStyleInputProps = {
   value?: SizeStyle
   imgProps: imgProps
   isImgAutoHeight: boolean
+  onRatioChange?: (value: number) => void
   onChange?: (value: SizeStyle) => void
 }
 
-const SizeStyleInput: React.VFC<SizeStyleInputProps> = ({ value, imgProps, isImgAutoHeight, onChange }) => {
+const SizeStyleInput: React.VFC<SizeStyleInputProps> = ({
+  value,
+  imgProps,
+  isImgAutoHeight,
+  onRatioChange,
+  onChange,
+}) => {
   const { formatMessage } = useIntl()
 
   return (
@@ -34,30 +39,27 @@ const SizeStyleInput: React.VFC<SizeStyleInputProps> = ({ value, imgProps, isImg
           value={value?.width === undefined ? imgProps.width : extractNumber(value?.width?.toString())}
           min={0}
           onChange={v => {
-            let newHeight = 0
-            if (typeof v === 'number') {
-              let newGcd = v / imgProps.widthAspect
-              newHeight = imgProps.heightAspect * newGcd
+            const currentWidthUnit = extractSizeUnit(value?.width?.toString())
+            const currentHeightUnit = extractSizeUnit(value?.height?.toString())
+            let newHeight
+            if (isImgAutoHeight) {
+              newHeight = `${(Number(v) / imgProps.originalImage.ratio)}${currentHeightUnit}`
+            } else if (currentWidthUnit === currentHeightUnit && ['px', 'em', 'vw'].includes(currentWidthUnit)) {
+              newHeight = `${convertToPx(v?.toString() || '0px') / imgProps.aspectRatio}${currentHeightUnit}`
+            } else {
+              newHeight = value?.height
             }
-            let originalHeight = value?.height === undefined ? imgProps.height : value?.height
             onChange?.({
               ...value,
-              width: Number(v) + (extractSizeUnit(value?.width?.toString()) || 'px'),
-              height: isImgAutoHeight
-                ? Number(newHeight) + (extractSizeUnit(value?.width?.toString()) || 'px')
-                : originalHeight,
+              width: `${Number(v).toFixed(0)}${extractSizeUnit(value?.width?.toString()) || 'px'}`,
+              height: newHeight,
             })
           }}
         />
         <Select
           defaultValue="px"
           value={extractSizeUnit(value?.width?.toString())}
-          onChange={v =>
-            onChange?.({
-              ...value,
-              width: extractNumber(value?.width?.toString()) + v,
-            })
-          }
+          onChange={v => onChange?.({ ...value, width: extractNumber(value?.width?.toString()) + v })}
           style={{ width: '70px' }}
         >
           <Select.Option value="px">px</Select.Option>
@@ -72,12 +74,21 @@ const SizeStyleInput: React.VFC<SizeStyleInputProps> = ({ value, imgProps, isImg
           value={value?.width !== undefined ? extractNumber(value?.height?.toString()) : imgProps.height}
           min={0}
           disabled={isImgAutoHeight}
-          onChange={v =>
+          onChange={v => {
+            const currentWidthUnit = extractSizeUnit(value?.width?.toString())
+            const currentHeightUnit = extractSizeUnit(value?.height?.toString())
+            let newWidth
+            if (currentWidthUnit === currentHeightUnit && ['px', 'em', 'vh'].includes(currentHeightUnit)) {
+              newWidth = `${convertToPx(v?.toString() || '0px') * imgProps.aspectRatio}${currentWidthUnit}`
+            } else {
+              newWidth = value?.width
+            }
             onChange?.({
               ...value,
-              height: Number(v) + (extractSizeUnit(value?.height?.toString()) || 'px'),
+              width: newWidth,
+              height: `${Number(v).toFixed(0)}${extractSizeUnit(value?.height?.toString())}`,
             })
-          }
+          }}
         />
         <Select
           defaultValue="px"
