13f18c78f0ee6f466649e5cd68874eafc1be9122
ShenDing1125 <shending1125@gmail.com>
2024-04-16 18:10:20 +0800

    feat: add functionality to navigate to memberNote URL

--- src/components/member/MemberNoteAdminModal.tsx
+++ src/components/member/MemberNoteAdminModal.tsx
@@ -233,7 +233,7 @@ const MemberNoteAdminModal: React.FC<
           label={formatMessage(memberMessages.label.noteForPermission)}
           name="note"
           initialValue={note?.note}
-          className={currentUserRole === 'app-owner' ? '' : 'd-none'}
+          className={permissions.MEMBER_NOTE_VIEW_EDIT ? '' : 'd-none'}
         >
           <Input.TextArea />
         </Form.Item>
