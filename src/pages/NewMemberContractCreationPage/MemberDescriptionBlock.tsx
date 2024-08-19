f978e90cda6d61c563f92db2a3e95e5098ce407f
Logan <clothe09986@gmail.com>
2024-08-19 17:10:26 +0800

    feat: contract custom product

--- src/pages/NewMemberContractCreationPage/MemberDescriptionBlock.tsx
+++ src/pages/NewMemberContractCreationPage/MemberDescriptionBlock.tsx
@@ -12,7 +12,10 @@ const MemberDescriptionBlock: React.FC<{
         <Descriptions title={<span>學生資料</span>} bordered className="mb-5" column={2}>
           <Descriptions.Item label="學員姓名">{member.name}</Descriptions.Item>
           <Descriptions.Item label="學員信箱">{member.email}</Descriptions.Item>
-          <Descriptions.Item label="付款備註">{member.paymentComment || ''}</Descriptions.Item>
+          <Descriptions.Item span={2} label="付款備註">
+            {member.paymentComment || ''}
+          </Descriptions.Item>
+          <Descriptions.Item label="是否為BG">{member.isBG ? '是' : '否'}</Descriptions.Item>
         </Descriptions>
       </div>
     )
