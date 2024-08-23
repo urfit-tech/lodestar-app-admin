38da96a71c04b57cb9108e188f28de5927ccb72d
ponchimeow <trancetoradora@gmail.com>
2024-08-23 16:59:10 +0800

    feat: sales lead api

--- src/hooks/sales.ts
+++ src/hooks/sales.ts
@@ -1,10 +1,9 @@
 import { useMutation, useQuery } from '@apollo/client'
 import { gql } from '@apollo/client'
 import moment from 'moment'
-import { sum, prop, sortBy } from 'ramda'
+import { sum } from 'ramda'
 import hasura from '../hasura'
-import { SalesProps, LeadProps, LeadStatus, Manager, GetSalesLeadMemberDataInfo } from '../types/sales'
-import { notEmpty } from '../helpers'
+import { SalesProps, LeadStatus, Manager, SalesLeadMember } from '../types/sales'
 import dayjs from 'dayjs'
 import { useCallback, useEffect, useMemo, useState } from 'react'
 import { useApp } from 'lodestar-app-element/src/contexts/AppContext'
@@ -273,17 +272,8 @@ export const useGetManagerWithMemberCount = (managerId: string, appId: string) =
 
 export const useManagerLeads = (manager: Manager) => {
   const { id: appId } = useApp()
-  const {
-    data: salesLeadMemberData,
-    error: errorMembers,
-    loading: loadingMembers,
-    refetch: refetchMembers,
-  } = useQuery<hasura.PhGetSalesLeadMembers, hasura.PhGetSalesLeadMembersVariables>(PhGetSalesLeadMembers, {
-    variables: { managerId: manager.id, appId },
-  })
   const { authToken } = useAuth()
-
-  const [salesLeadMemberInfo, setSalesLeadMemberInfo] = useState<GetSalesLeadMemberDataInfo>()
+  const [salesLeadMembers, setSalesLeadMembers] = useState<SalesLeadMember[]>([])
   const [error, setError] = useState<any>(null)
   const [loading, setLoading] = useState(false)
 
@@ -293,18 +283,30 @@ export const useManagerLeads = (manager: Manager) => {
 
     if (authToken && manager && manager.id && appId) {
       try {
-        const payload = {
-          managerId: manager.id,
-          appId: appId,
-        }
-
-        const { data } = await axios.post(
-          `${process.env.REACT_APP_LODESTAR_SERVER_ENDPOINT}/members/saleLeadMemberData`,
-          payload,
-          { headers: { authorization: `Bearer ${authToken}` } },
-        )
-
-        setSalesLeadMemberInfo(data)
+        await axios
+          .post(
+            `${process.env.REACT_APP_KOLABLE_SERVER_ENDPOINT}/kolable/sales-lead/${manager.id}`,
+            {},
+            {
+              headers: { authorization: `Bearer ${authToken}` },
+            },
+          )
+          .then(({ data }) => {
+            const result = data.map((salesLeadMember: SalesLeadMember) => ({
+              ...salesLeadMember,
+              createdAt: dayjs(salesLeadMember.createdAt).toDate(),
+              assignedAt:salesLeadMember.assignedAt ? dayjs(salesLeadMember.assignedAt).toDate() :null,
+              followedAt: salesLeadMember.followedAt ? dayjs(salesLeadMember.followedAt).toDate() : null,
+              closedAt: salesLeadMember.closedAt ? dayjs(salesLeadMember.closedAt).toDate() : null,
+              completedAt: salesLeadMember.completedAt ? dayjs(salesLeadMember.completedAt).toDate() : null,
+              excludedAt: salesLeadMember.excludedAt ? dayjs(salesLeadMember.excludedAt).toDate() : null,
+              recycledAt: salesLeadMember.recycledAt ? dayjs(salesLeadMember.recycledAt).toDate() : null,
+              recentContactedAt: salesLeadMember.recentContactedAt ? dayjs(salesLeadMember.recentContactedAt).toDate() : null,
+              recentAnsweredAt: salesLeadMember.recentAnsweredAt ? dayjs(salesLeadMember.recentAnsweredAt).toDate() : null,
+            }))
+            setSalesLeadMembers(result)
+          })
+          .catch(error => setError(error))
       } catch (error) {
         setError(error)
       } finally {
@@ -319,126 +321,24 @@ export const useManagerLeads = (manager: Manager) => {
     fetchData()
   }, [fetchData])
 
-  const refetch = () => {
-    fetchData()
-  }
-  const convertToLead = (v: hasura.PhGetSalesLeadMembers['member'][number] | null): LeadProps | null => {
-    if (!v || v.member_phones.length === 0) {
-      return null
-    }
-    const signed = Number(salesLeadMemberInfo?.activeMemberContract?.filter(mc => mc.memberId === v.id).length) > 0
-    const status: LeadStatus = v.followed_at
-      ? 'FOLLOWED'
-      : signed
-      ? 'SIGNED'
-      : v.excluded_at
-      ? 'DEAD'
-      : v.closed_at
-      ? 'CLOSED'
-      : v.completed_at
-      ? 'COMPLETED'
-      : salesLeadMemberInfo?.memberTask?.find(mt => mt.memberId === v.id)?.status === 'done'
-      ? 'PRESENTED'
-      : salesLeadMemberInfo?.memberTask?.find(mt => mt.memberId === v.id) //member has member_task => INVITED demo
-      ? 'INVITED'
-      : v.last_member_note_answered
-      ? 'ANSWERED'
-      : v.last_member_note_called
-      ? 'CONTACTED'
-      : 'IDLED'
-    return {
-      appId: v.app_id,
-      id: v.id,
-      star: v.star,
-      name: v.name,
-      email: v.email,
-      pictureUrl: v.picture_url || '',
-      createdAt: moment(v.created_at).toDate(),
-      phones: v.member_phones?.map(_v => ({ phoneNumber: _v.phone, isValid: _v.is_valid })),
-      notes: salesLeadMemberInfo?.memberNote?.filter(mn => mn.memberId === v.id)[0]?.description || '',
-      categoryNames:
-        salesLeadMemberInfo?.memberCategory.filter(mc => mc.memberId === v.id).map(_v => _v.name || '') || [],
-      properties:
-        salesLeadMemberInfo?.memberProperty
-          ?.filter(mp => mp.memberId === v.id)
-          .map(v => ({
-            id: v.propertyId || '',
-            name: v.name || '',
-            value: v.value,
-          })) || [],
-      status,
-      leadStatusCategoryId: v.lead_status_category_id,
-      assignedAt: v.assigned_at ? dayjs(v.assigned_at).toDate() : null,
-      notified: !v.last_member_note_created,
-      recentContactedAt: v.last_member_note_called ? dayjs(v.last_member_note_called).toDate() : null,
-      recentAnsweredAt: v.last_member_note_answered ? dayjs(v.last_member_note_answered).toDate() : null,
-      completedAt: v.completed_at ? dayjs(v.completed_at).toDate() : null,
-      closedAt: v.closed_at ? dayjs(v.closed_at).toDate() : null,
-      followedAt: v.followed_at ? dayjs(v.followed_at).toDate() : null,
-      excludedAt: v.excluded_at ? dayjs(v.excluded_at).toDate() : null,
-      recycledAt: v.recycled_at ? dayjs(v.recycled_at).toDate() : null,
-    }
-  }
-
-  const totalLeads: LeadProps[] = sortBy(prop('id'))(
-    salesLeadMemberData?.member.map(convertToLead)?.filter(notEmpty) || [],
-  )
+  const refetch = () => fetchData()
 
   return {
     loading,
     error,
     refetch,
-    loadingMembers,
-    errorMembers,
-    refetchMembers,
-    totalLeads,
-    followedLeads: totalLeads.filter(lead => lead.status === 'FOLLOWED'),
-    idledLeads: totalLeads.filter(lead => lead.status === 'IDLED'),
-    contactedLeads: totalLeads.filter(lead => lead.status === 'CONTACTED'),
-    answeredLeads: totalLeads.filter(lead => lead.status === 'ANSWERED'),
-    invitedLeads: totalLeads.filter(lead => lead?.status === 'INVITED'),
-    presentedLeads: totalLeads.filter(lead => lead?.status === 'PRESENTED'),
-    signedLeads: totalLeads.filter(lead => lead?.status === 'SIGNED'),
-    closedLeads: totalLeads.filter(lead => lead?.status === 'CLOSED'),
-    completedLeads: totalLeads.filter(lead => lead?.status === 'COMPLETED'),
+    totalLeads: salesLeadMembers,
+    followedLeads: salesLeadMembers.filter((lead: { status: string }) => lead.status === 'FOLLOWED'),
+    idledLeads: salesLeadMembers.filter((lead: { status: string }) => lead.status === 'IDLED'),
+    contactedLeads: salesLeadMembers.filter((lead: { status: string }) => lead.status === 'CONTACTED'),
+    answeredLeads: salesLeadMembers.filter((lead: { status: string }) => lead.status === 'ANSWERED'),
+    invitedLeads: salesLeadMembers.filter((lead: { status: string }) => lead?.status === 'INVITED'),
+    presentedLeads: salesLeadMembers.filter((lead: { status: string }) => lead?.status === 'PRESENTED'),
+    signedLeads: salesLeadMembers.filter((lead: { status: string }) => lead?.status === 'SIGNED'),
+    closedLeads: salesLeadMembers.filter((lead: { status: string }) => lead?.status === 'CLOSED'),
+    completedLeads: salesLeadMembers.filter((lead: { status: string }) => lead?.status === 'COMPLETED'),
   }
 }
-
-const PhGetSalesLeadMembers = gql`
-  query PhGetSalesLeadMembers($appId: String!, $managerId: String!) {
-    member(
-      where: {
-        app_id: { _eq: $appId }
-        manager_id: { _eq: $managerId }
-        member_phones: { phone: { _is_null: false } }
-        excluded_at: { _is_null: true }
-        _or: [{ star: { _gte: -9999 } }, { star: { _is_null: true } }]
-      }
-    ) {
-      app_id
-      id
-      name
-      email
-      picture_url
-      star
-      created_at
-      assigned_at
-      followed_at
-      closed_at
-      completed_at
-      excluded_at
-      recycled_at
-      last_member_note_created
-      last_member_note_called
-      last_member_note_answered
-      lead_status_category_id
-      member_phones {
-        phone
-        is_valid
-      }
-    }
-  }
-`
 
 const GetManagerWithMemberCount = gql`
   query GetManagerMemberCount($appId: String!, $managerId: String!) {
