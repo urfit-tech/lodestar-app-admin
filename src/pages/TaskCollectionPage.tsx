567ee7e1029ae9b0ec8502ddf5066d9893d44e00
Logan (clothe09986) <64723534+clothe09986@users.noreply.github.com>
2024-08-26 01:41:13 +1000

    Merge pull request #556 from urfit-tech/feat-member-task-link
    feat: member task collection add link

--- src/pages/TaskCollectionPage.tsx
+++ src/pages/TaskCollectionPage.tsx
@@ -2,15 +2,19 @@ import { UserOutlined } from '@ant-design/icons'
 import { useAuth } from 'lodestar-app-element/src/contexts/AuthContext'
 import React from 'react'
 import { useIntl } from 'react-intl'
+import { StringParam, useQueryParam } from 'use-query-params'
 import { AdminPageTitle } from '../components/admin'
 import AdminLayout from '../components/layout/AdminLayout'
 import MemberTaskAdminBlock from '../components/task/MemberTaskAdminBlock'
 import { commonMessages } from '../helpers/translation'
+import { useMemberTask } from '../hooks/task'
 import ForbiddenPage from './ForbiddenPage'
 
 const TaskCollectionPage: React.FC = () => {
   const localStorageMemberTaskDisplay = localStorage.getItem('memberTaskDisplay') || undefined
   const localStorageMemberTaskFilter = JSON.parse(localStorage.getItem('memberTaskFilter') || 'null')
+  const [activeMemberTaskId] = useQueryParam('id', StringParam)
+  const { memberTask } = useMemberTask(activeMemberTaskId || '')
 
   const { formatMessage } = useIntl()
   const { permissions } = useAuth()
@@ -29,6 +33,7 @@ const TaskCollectionPage: React.FC = () => {
       <MemberTaskAdminBlock
         localStorageMemberTaskDisplay={localStorageMemberTaskDisplay}
         localStorageMemberTaskFilter={localStorageMemberTaskFilter}
+        activeMemberTask={memberTask}
       />
     </AdminLayout>
   )
