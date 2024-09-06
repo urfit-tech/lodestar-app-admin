56f66330da61c2ff13bab6001fd6ec4d302e18cd
Logan <clothe09986@gmail.com>
2024-09-06 12:22:28 +0800

    fix: invoice text

--- src/components/sale/InvoiceCard.tsx
+++ src/components/sale/InvoiceCard.tsx
@@ -308,7 +308,7 @@ const InvoiceCard: React.FC<{
                         randomNumber: invoiceResponse?.RandomNum,
                         sellerUniformNumber: companyUniformNumber,
                         totalPrice: Number(invoiceResponse?.TotalAmt || 0).toLocaleString(),
-                        uniformTitle: invoiceResponse?.BuyerUBN && `賣方 ${invoiceResponse.BuyerUBN}`,
+                        uniformTitle: invoiceResponse?.BuyerUBN && `買方 ${invoiceResponse.BuyerUBN}`,
                         invoiceNo: `${invoiceResponse?.InvoiceNumber.substring(
                           0,
                           2,
