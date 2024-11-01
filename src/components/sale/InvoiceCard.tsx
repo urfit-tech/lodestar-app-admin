e997fb7d576da7762849f75ca6b193720d6ced18
Logos as a philosopher <yures611@gmail.com>
2024-11-01 18:44:12 +0800

    fix: year and month of printed invoice should follow created time

--- src/components/sale/InvoiceCard.tsx
+++ src/components/sale/InvoiceCard.tsx
@@ -5,7 +5,7 @@ import { useApp } from 'lodestar-app-element/src/contexts/AppContext'
 import { useAuth } from 'lodestar-app-element/src/contexts/AuthContext'
 import { handleError } from 'lodestar-app-element/src/helpers'
 import { render } from 'mustache'
-import { sum } from 'ramda'
+import { defaultTo, pipe, prop, sum } from 'ramda'
 import React, { useRef, useState } from 'react'
 import { useIntl } from 'react-intl'
 import styled from 'styled-components'
@@ -282,6 +282,8 @@ const InvoiceCard: React.FC<{
     '12': '11-12',
   }
 
+  console.log(invoiceResponse)
+  const getMinGuoYear = (date: Date) => date.getFullYear() - 1911
   const currentMonth = new Date().getMonth() + 1
 
   return (
@@ -309,8 +311,18 @@ const InvoiceCard: React.FC<{
                       ref={receiptRef1}
                       template={JSON.parse(settings['invoice.template'])?.main || ''}
                       templateVariables={{
-                        year: new Date().getFullYear() - 1911,
-                        month: INVOICE_RANGE[currentMonth.toString()],
+                        year: pipe(
+                          (prop as any)('CreateTime'),
+                          (defaultTo as any)(new Date()),
+                          (_: string | Date) => new Date(_),
+                          getMinGuoYear,
+                        )(invoiceResponse),
+                        month:
+                          INVOICE_RANGE[
+                            invoiceResponse?.CreateTime
+                              ? (new Date(invoiceResponse?.CreateTime).getMonth() + 1).toString()
+                              : currentMonth.toString()
+                          ],
                         createdAt: invoiceResponse?.CreateTime,
                         randomNumber: invoiceResponse?.RandomNum,
                         sellerUniformNumber: companyUniformNumber,
