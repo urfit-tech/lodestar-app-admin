e3291b94d885dfd0d8b4e3143370ce21d7b3fdae
ponchimeow <trancetoradora@gmail.com>
2024-12-24 21:28:38 +0800

    feat:allow both images and embedded

--- src/components/issue/IssueReplyCreationBlock.tsx
+++ src/components/issue/IssueReplyCreationBlock.tsx
@@ -80,7 +80,7 @@ const IssueReplyCreationBlock: React.FC<{
           media={{
                 uploadFn: createUploadFn(appId, authToken),
                 accepts: { video: false, audio: false },
-                externals: { image: true, video: false, audio: false, embed: false },
+                externals: { image: true, video: false, audio: false, embed: true },
               }}
         />
       </Form.Item>
