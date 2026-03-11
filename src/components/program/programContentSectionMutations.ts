ddb3a0ca941d72e4a5adffc3568aa6989719d96e
Eddy <me@eddy.today>
2026-03-11 16:37:05 +0800

    feat: implement delete program content section mutation
    - Added DELETE_PROGRAM_CONTENT_SECTION mutation to handle the deletion of various program content types associated with a content section.
    - Removed the old DELETE_PROGRAM_CONTENT_SECTION mutation from ProgramContentSectionAdminCard.tsx for cleaner code organization.

---
+++ src/components/program/programContentSectionMutations.ts
@@ -0,0 +1,55 @@
+import { gql } from '@apollo/client'
+
+export const DELETE_PROGRAM_CONTENT_SECTION = gql`
+  mutation DELETE_PROGRAM_CONTENT_SECTION($programContentSectionId: uuid!) {
+    delete_program_content_plan(
+      where: { program_content: { content_section_id: { _eq: $programContentSectionId } } }
+    ) {
+      affected_rows
+    }
+    delete_program_content_progress(
+      where: { program_content: { content_section_id: { _eq: $programContentSectionId } } }
+    ) {
+      affected_rows
+    }
+    delete_practice(where: { program_content: { content_section_id: { _eq: $programContentSectionId } } }) {
+      affected_rows
+    }
+    delete_exercise(where: { program_content: { content_section_id: { _eq: $programContentSectionId } } }) {
+      affected_rows
+    }
+    delete_program_content_log(
+      where: { program_content: { content_section_id: { _eq: $programContentSectionId } } }
+    ) {
+      affected_rows
+    }
+    delete_program_content_video(
+      where: { program_content: { content_section_id: { _eq: $programContentSectionId } } }
+    ) {
+      affected_rows
+    }
+    delete_program_content_audio(
+      where: { program_content: { content_section_id: { _eq: $programContentSectionId } } }
+    ) {
+      affected_rows
+    }
+    delete_program_content_ebook_bookmark(
+      where: { program_content: { content_section_id: { _eq: $programContentSectionId } } }
+    ) {
+      affected_rows
+    }
+    delete_program_content_ebook_highlight(
+      where: { program_content: { content_section_id: { _eq: $programContentSectionId } } }
+    ) {
+      affected_rows
+    }
+    delete_program_content_body(
+      where: { program_contents: { content_section_id: { _eq: $programContentSectionId } } }
+    ) {
+      affected_rows
+    }
+    delete_program_content_section(where: { id: { _eq: $programContentSectionId } }) {
+      affected_rows
+    }
+  }
+`
